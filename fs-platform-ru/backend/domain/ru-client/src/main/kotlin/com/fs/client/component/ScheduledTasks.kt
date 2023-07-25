c787efc27b914ed2d437439affc5ad0afd399003
mallarion <Mallarion0521@gmail.com>
2023-07-25 13:45:57 +0300

    fix(ru-client): add crontab for eventRepository

--- fs-platform-ru/backend/domain/ru-client/src/main/kotlin/com/fs/client/component/ScheduledTasks.kt
+++ fs-platform-ru/backend/domain/ru-client/src/main/kotlin/com/fs/client/component/ScheduledTasks.kt
@@ -1,15 +1,21 @@
 package com.fs.client.component
 
+import com.fs.client.repository.EventRepository
 import com.fs.client.repository.OrderRepository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 
 @Component
-open class ScheduledTasks(open val orderRepository: OrderRepository) {
+open class ScheduledTasks(
+    open val orderRepository: OrderRepository,
+    open val eventRepository: EventRepository
+    ) {
     @Scheduled(cron = EXPIRED_STATUS_CRON_SCHEDULER)
     fun reportCurrentTime() {
         orderRepository.updateExpiredStatus()
+        eventRepository.updateExpiredEventsStatus()
+
     }
 
     companion object {
