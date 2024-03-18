d203cff04b44361b8366c36a646ab4e08c5f4a5c
LizzieMonk <90979377+LizzieMonk@users.noreply.github.com>
2024-03-18 14:24:45 +0300

    first commit (#26)

--- frontend/public/constants/url.js
+++ frontend/public/constants/url.js
@@ -1,8 +1,8 @@
 // For prod
-//  const BASE_URL = "http://45.135.234.61:8183/graphql"
+ const BASE_URL = "http://45.135.234.61:8183/graphql"
 //  const BASE_URL = "http://funscrut.online:8183/graphql"
 
-// For dev
-const BASE_URL = "http://localhost:8183/graphql"
+// // For dev
+// const BASE_URL = "http://localhost:8183/graphql"
 
 export default BASE_URL
