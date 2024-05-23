a402e92ee7442b2004b7a76c7e792a1f7201162c
nikolay-klekto <funScrut@gmail.com>
2024-05-23 17:14:07 +0300

    FF-4 fix(ru-client): write metrics logic [deploy-back]

--- backend/domain/ru-client/src/main/kotlin/com/fs/client/config/MetricsConfig.kt
+++ backend/domain/ru-client/src/main/kotlin/com/fs/client/config/MetricsConfig.kt
@@ -1,15 +1,15 @@
 package com.fs.client.config
 
-import io.micrometer.core.aop.TimedAspect
-import io.micrometer.core.instrument.MeterRegistry
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class MetricsConfig(private val registry: MeterRegistry) {
-
-    @Bean
-    fun timedAspect(): TimedAspect {
-        return TimedAspect(registry)
-    }
-}
+//import io.micrometer.core.aop.TimedAspect
+//import io.micrometer.core.instrument.MeterRegistry
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//class MetricsConfig(private val registry: MeterRegistry) {
+//
+//    @Bean
+//    fun timedAspect(): TimedAspect {
+//        return TimedAspect(registry)
+//    }
+//}
