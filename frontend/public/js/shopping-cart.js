f7faf83a0e5b3eeef769fadb1fe1774cb96552fa
NatUsik6 <tashausik6@gmail.com>
2024-08-07 12:51:04 +0300

    feat: implement request to "server"

--- frontend/public/js/shopping-cart.js
+++ frontend/public/js/shopping-cart.js
@@ -19,16 +19,77 @@ function disableDropdownMenuOpen() {
   document.body.style.overflow = "";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const deleteButtons = document.querySelectorAll('.card__content__item__button-trashcan, .card__button-trashcan');
+const deleteButtons = document.querySelectorAll(
+  ".card__content__item__button-trashcan, .card__button-trashcan"
+);
 
-  deleteButtons.forEach(button => {
-    button.addEventListener("click", (event) => {
-      const cardId = event.currentTarget.getAttribute('data-card-id');
-      const card = document.getElementById(cardId);
-      if (card) {
-        card.remove();
-      }
-    });
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const cardId = event.currentTarget.getAttribute("data-card-id");
+    const card = document.getElementById(cardId);
+    if (card) {
+      card.remove();
+    }
   });
 });
+
+const orderButtons = document.querySelectorAll(
+  ".card__content__item__button-order, .card__button-order"
+);
+
+orderButtons.forEach((button) => {
+  button.addEventListener("click", async (event) => {
+    const cardId = event.currentTarget.getAttribute("data-card-id");
+    const card = document.getElementById(cardId);
+
+    if (card) {
+      const cardProps = card.querySelectorAll(
+        ".card__content__item__description"
+      );
+
+      const internshipStartParts = cardProps[2].textContent.split(".");
+      const internshipStart = new Date(
+        internshipStartParts[2],
+        internshipStartParts[1] - 1,
+        internshipStartParts[0]
+      );
+      const internshipFinishParts = cardProps[3].textContent.split(".");
+      const internshipFinish = new Date(
+        internshipFinishParts[2],
+        internshipFinishParts[1] - 1,
+        internshipFinishParts[0]
+      );
+
+      const request = {
+        profession: cardProps[0].textContent,
+        company: cardProps[1].textContent,
+        internshipStart: internshipStart,
+        internshipFinish: internshipFinish,
+        internshipType: cardProps[4].textContent,
+        officeAddress: cardProps[5].textContent,
+        price: +cardProps[6].textContent.split(" ")[0],
+      };
+
+      const result = await sendRequest(request);
+      console.log(result);
+    }
+  });
+});
+
+async function sendRequest(request) {
+  let {
+    profession,
+    company,
+    internshipStart,
+    internshipFinish,
+    internshipType,
+    officeAddress,
+    price,
+  } = request;
+
+  console.log(
+    `profession: ${profession}; company: ${company}; internshipStart: ${internshipStart}; internshipFinish: ${internshipFinish}; internshipType: ${internshipType}; officeAddress: ${officeAddress}; price: ${price}`
+  );
+
+  return true;
+}
