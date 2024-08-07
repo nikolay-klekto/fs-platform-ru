235f021ea011fe6b33cb7d3c7b841dd359d88ebb
Semio@Semion <obucheniekurs7@gmail.com>
2024-08-07 12:28:50 +0300

    FFO-2 feat: add logic for the bin, collapse dropdown list logic, server request

--- frontend/public/pages/personal-account/js/index.js
+++ frontend/public/pages/personal-account/js/index.js
@@ -15,19 +15,25 @@ window.addEventListener('resize', checkMediaQuery);
 
 
 const personalAccountMenuDropdownButton = document.querySelector('.personal-account__menu-dropdown-button');
-const personalAccountMenuDropdownItem = document.querySelectorAll('.personal-account__menu-dropdown-item');
+const personalAccountMenuDropdownItems = document.querySelectorAll('.personal-account__menu-dropdown-item');
 const personalAccountMenuDropdownList = document.querySelector('.personal-account__menu-dropdown-list');
 const dropdownButton = document.querySelector('.dropdown__button');
 const personalAccountMenuDropdownInput = document.querySelector('.personal-account__menu-dropdown-input');
+const personalAccountCartCatalogItemRemoveCards = document.querySelectorAll('.personal-account__cart-catalog-item-remove-card');
+const orderPlacingCartButton = document.querySelectorAll('.order-placing__cart-button');
 
 //Click on the button. Open/Close select
 personalAccountMenuDropdownButton.addEventListener('click', function (e) {
-    e.stopPropagation();
-    personalAccountMenuDropdownList.style.display = 'block';
+    if (personalAccountMenuDropdownList.style.display === 'block') {
+        personalAccountMenuDropdownList.style.display = 'none';
+    } else {
+        e.stopPropagation();
+        personalAccountMenuDropdownList.style.display = 'block';
+    }
 });
 
 //Select list item. Remember selected value. Close dropdown
-personalAccountMenuDropdownItem.forEach(function (listItem) {
+personalAccountMenuDropdownItems.forEach(function (listItem) {
     listItem.addEventListener('click', function (e) {
         e.stopPropagation();
         dropdownButton.innerText = this.innerText;
@@ -37,6 +43,100 @@ personalAccountMenuDropdownItem.forEach(function (listItem) {
 });
 
 //Close dropdown
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function () {
     personalAccountMenuDropdownList.style.display = 'none';
+});
+
+//Close order card
+personalAccountCartCatalogItemRemoveCards.forEach(function (listItem) {
+    listItem.addEventListener('click', function () {
+        listItem.parentElement.remove();
+    })
+});
+
+
+async function checkout (obj){
+    //смоделированный запрос на сервер
+
+    let { profession, company, startDate, endDate, type, address, price } = obj;
+
+    const query = await sendQuery(`
+        mutation {
+            checkout (
+                obj: [{
+                    profession : ${profession},
+                    company : ${company},
+                    startDate : ${startDate},
+                    endDate : ${endDate},
+                    type : ${type},
+                    address : ${address},
+                    price : ${price},
+                }]
+            ){
+                dataModel{
+                    profession
+                    company
+                    startDate
+                    endDate
+                    type
+                    address
+                    price
+                }
+                errors
+            }
+        }
+    `)
+
+    console.log("profession:", profession);
+    console.log("company:", company);
+    console.log("startDate:", startDate);
+    console.log("endDate:", endDate);
+    console.log("type:", type);
+    console.log("address:", address);
+    console.log("price:", price);
+
+    const result = true;
+    console.log(result);
+
+    return result;
+}
+
+async function sendQuery (queryBody){
+    try{
+        // 'http://100.100.100.100:3000/graphql'
+        const response = await fetch (URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({query: queryBody}),
+        })
+        if(response.ok){
+            const { data } = await response.json();
+            return { data, response };
+        } else {
+            throw new Error (response.status);
+        }
+    }
+    catch(error){
+        error.message;
+    }
+}
+
+//Click on order place
+orderPlacingCartButton.forEach(function (listItem) {
+    listItem.addEventListener('click', function () {
+        const cartItem = this.closest('.personal-account__cart-catalog-item');
+        let order = {
+            profession: cartItem.querySelector('#profession').dataset.value,
+            company: cartItem.querySelector('#company').dataset.value,
+            startDate: new Date(cartItem.querySelector('#start-date').dataset.value),
+            endDate: new Date(cartItem.querySelector('#end-date').dataset.value),
+            type: cartItem.querySelector('#type').dataset.value,
+            address: cartItem.querySelector('#address').dataset.value,
+            price: Number(cartItem.querySelector('#price').dataset.value)
+        };
+        checkout(order);
+    })
 });
