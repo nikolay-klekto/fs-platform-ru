6359225ce58cad98c6ed2674cbbdd253ab150753
LeonovEgorF <egor1908608@gmail.com>
2024-10-23 16:44:47 +0300

    upd images

--- frontend-react/components/mobi/pageMobi/HomePageMobi/HomePageMobi.tsx
+++ frontend-react/components/mobi/pageMobi/HomePageMobi/HomePageMobi.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { TestIcon } from '@/components/asssets/icons'
 const HomePageMobi: React.FC = () => {
-    return <h1>HomePageMobi</h1>
+    return (
+        <h1>
+            <TestIcon />
+        </h1>
+    )
 }
 export default HomePageMobi
