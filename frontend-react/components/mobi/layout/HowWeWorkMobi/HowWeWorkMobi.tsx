468fbafc5ea120161aa1e43f3ab7b0d3938fddcc
LeonovEgorF <egor1908608@gmail.com>
2024-10-25 16:15:06 +0300

    upd

--- frontend-react/components/mobi/layout/HowWeWorkMobi/HowWeWorkMobi.tsx
+++ frontend-react/components/mobi/layout/HowWeWorkMobi/HowWeWorkMobi.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { content } from '@/components/mobi/layout/HowWeWorkMobi/content'
 import TitleMobi from '@/components/mobi/shared/TitleMobi'
 import SubTitleMobi from '@/components/mobi/shared/SubTitleMobi'
+import { Button } from '@/components/ui/button'
 
 const HowWeWorkMobi: React.FC = () => {
     return (
@@ -9,7 +10,7 @@ const HowWeWorkMobi: React.FC = () => {
             <div className="mb-[24px]">
                 <TitleMobi title={'Как мы работаем'} />
             </div>
-            <div>
+            <div className="mb-[24px]">
                 {content.map((item) => (
                     <div key={item.id}>
                         <div className="mt-[15px pb-[10px] pt-[20px] text-9xl text-[#353652] ">{item.num}</div>
@@ -23,6 +24,11 @@ const HowWeWorkMobi: React.FC = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="flex justify-center">
+                <Button variant="select_mobi" size="select_mobi">
+                    Развернуть
+                </Button>
             </div>
         </div>
     )
