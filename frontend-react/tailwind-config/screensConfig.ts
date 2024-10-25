65da9a2fef477af61faae1f1dd9ee8e669c374db
LeonovEgorF <egor1908608@gmail.com>
2024-10-25 12:25:17 +0300

    add rules style

---
+++ frontend-react/tailwind-config/screensConfig.ts
@@ -0,0 +1,11 @@
+const screensConfig = {
+    'sm': { max: '320px' },
+    'sm_l': { max: '375px' },
+    'sm_xl': { max: '425px' },
+    'md': { max: '768px' },
+    'lg': { max: '1024px' },
+    'xl': { max: '1280px' },
+    '2xl': { max: '1440px' },
+    '3xl': { max: '1560px' },
+}
+export default screensConfig
