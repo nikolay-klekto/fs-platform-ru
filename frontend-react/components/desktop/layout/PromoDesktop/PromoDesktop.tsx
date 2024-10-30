38807c6b51393d7ce410d27b3a0bd0472cb5f937
AlenaBukhtarevich <alena89xxx@gmail.com>
2024-10-30 14:18:44 +0300

    FF-29-promo/correct fontSize, screens lg and xl

--- frontend-react/components/desktop/layout/PromoDesktop/PromoDesktop.tsx
+++ frontend-react/components/desktop/layout/PromoDesktop/PromoDesktop.tsx
@@ -9,19 +9,19 @@ const PromoDesktop: React.FC = () => {
                 backgroundImage: 'url(/background/background-promo.jpg)',
                 backgroundSize: 'cover',
             }}
-            className="flex items-center justify-center p-6 h-[650px]"
+            className="flex lg:flex-col xl:flex-col items-center justify-center p-6 h-[650px]"
         >
             <div className="max-w-[68rem]">
-                <h3 className="lg:text-[32px] xl:text-[40px] text-[46px] font-semibold text-white">
+                <h3 className="lg:text-9xl xl:text-15xl text-18xl font-semibold text-white">
                     РЕГИСТРИРУЙСЯ, ВЫБИРАЙ ПРОФЕССИЮ И КОМПАНИЮ, ПОЛУЧАЙ НОВЫЕ ЗНАНИЯ И ОПЫТ
                 </h3>
-                <p className="lg:text-[18px] xl:text-[20px] text-[22px] font-medium text-white mt-4 mt-6">
+                <p className="lg:text-3xl xl:text-5xl text-6xl font-medium text-white mt-4 mt-6">
                     Поможем пройти стажировку в любой интересующей профессии или компании, независимо от наличия опыта и
                     навыков
                 </p>
             </div>
-            <div className="flex space-x-4 justify-center">
-                <Button className="bg-white hover:bg-white hover:shadow-lg hover:shadow-[#3B51A8] text-[35px] font-semibold rounded-[45px] w-[500px] h-[95px]">
+            <div className="flex space-x-4 justify-center lg:mt-10 xl:mt-10">
+                <Button className="bg-white hover:bg-white hover:shadow-lg hover:shadow-[#3B51A8] lg:text-9xl text-13xl font-semibold rounded-[45px] w-[500px] h-[95px]">
                     <span className="bg-gradient-to-r from-[#8333F3] to-[#3B51A8] bg-clip-text text-transparent">
                         Зарегистрироваться
                     </span>
