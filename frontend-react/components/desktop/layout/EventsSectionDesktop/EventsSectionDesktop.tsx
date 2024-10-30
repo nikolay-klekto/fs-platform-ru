0d92378827403af01ee427a86a090e471c648c0b
Daria <scheglova.daria@gmail.com>
2024-10-31 02:31:41 +0300

    add: custom scrollbar

--- frontend-react/components/desktop/layout/EventsSectionDesktop/EventsSectionDesktop.tsx
+++ frontend-react/components/desktop/layout/EventsSectionDesktop/EventsSectionDesktop.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import TitleDesktop from '@/components/desktop/shared/TitleDesktop'
 import ItemEventsDesktop from '@/components/desktop/layout/EventsSectionDesktop/ItemEventsDesktop/ItemEventsDesktop'
 import { content } from '@/components/desktop/layout/EventsSectionDesktop/content'
 const EventsSectionDesktop: React.FC = () => {
+    const contentRef = useRef<HTMLDivElement>(null)
+    const scrollbarRef = useRef<HTMLDivElement>(null)
+
+    const handleScroll = () => {
+        if (contentRef.current && scrollbarRef.current) {
+            scrollbarRef.current.scrollLeft = contentRef.current.scrollLeft
+        }
+    }
+
+    const handleScrollbarScroll = () => {
+        if (contentRef.current && scrollbarRef.current) {
+            contentRef.current.scrollLeft = scrollbarRef.current.scrollLeft
+        }
+    }
     return (
         <div className="pb-[100px] pt-[23px]">
             <TitleDesktop title={'МЕРОПРИЯТИЯ'} />
-            <div className="ml-[47px]  flex gap-[34px] overflow-x-auto scrollbar_custom">
+            <div
+                ref={contentRef}
+                onScroll={handleScroll}
+                className="ml-[47px]  flex gap-[34px] overflow-x-scroll no-scrollbar_custom"
+            >
                 {content.map((item) => (
                     <ItemEventsDesktop image={item.image} title={item.title} date={item.date} key={item.id} />
                 ))}
+            </div>
+            <div
+                ref={scrollbarRef}
+                onScroll={handleScrollbarScroll}
+                className="h-2 w-[60%] mx-auto overflow-x-scroll scrollbar_custom"
+            >
+                <div className="w-[250%] h-full"></div>
             </div>
         </div>
     )
--- frontend-react/components/desktop/layout/EventsSectionDesktop/ItemEventsDesktop/ItemEventsDesktop.tsx
+++ frontend-react/components/desktop/layout/EventsSectionDesktop/ItemEventsDesktop/ItemEventsDesktop.tsx
@@ -11,15 +11,15 @@ const ItemEventsDesktop: React.FC<EventSectionDesktopProps> = ({ image, title, d
     const [day, month] = date.split(' ')
 
     return (
-        <div className="pb-[30px]">
+        <div className="pb-[100px]">
             <div className="relative min-h-[454px] min-w-[520px]">
                 <Image src={image} fill alt={title} className="object-cover rounded-[50px]" />
-                <div className="absolute right-[23px] top-[18px] w-[114px] h-[101px] rounded-[23px] bg-white bg-opacity-90 flex flex-col justify-center items-center">
+                <div className="absolute right-[23px] top-[18px] w-[114px] h-[101px] rounded-[23px] bg-white bg-opacity-80 flex flex-col justify-center items-center">
                     <p className="text-9xl font-semibold text-[#1f203f]">{day}</p>
                     <p className="text-4xl font-medium text-[#878797]">{month}</p>
                 </div>
             </div>
-            <p className="uppercase text-9xl font-medium text-white pt-5">{title}</p>
+            <p className="uppercase text-9xl font-medium text-white pt-5 min-h-[64px] max-h-[64px]">{title}</p>
         </div>
     )
 }
