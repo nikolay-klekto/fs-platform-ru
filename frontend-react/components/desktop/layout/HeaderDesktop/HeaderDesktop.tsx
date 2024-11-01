12339ade15d404ac6efbc32ef1e9a82be7c22b55
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-11-01 12:38:39 +0300

    feat: add mobile adaptiv add burger menu

--- frontend-react/components/desktop/layout/HeaderDesktop/HeaderDesktop.tsx
+++ frontend-react/components/desktop/layout/HeaderDesktop/HeaderDesktop.tsx
@@ -11,15 +11,15 @@ const HeaderDesktop: React.FC = () => {
     return (
         <>
             <header
-                className="relative flex justify-center pt-[34px] pb-[35px] px-[59px] bg-[rgba(16,16,48,0.7)] border border-red-500 border-solid"
+                className="relative flex justify-center pt-[34px] pb-[35px] px-[59px] bg-[rgba(16,16,48,0.7)]"
                 style={{
                     backgroundImage: 'url(/background/colorBackground.png)',
                     backgroundSize: 'cover',
                 }}
             >
                 <div className="absolute inset-0 bg-[rgba(16,16,48,0.7)]"></div>
-                <div className="relative w-full max-w-[1800px] justify-between flex items-center z-10 border border-red-500 border-solid">
-                    <div className="flex flex-col justify-center items-center border border-red-500 border-solid">
+                <div className="relative w-full max-w-[1800px] justify-between flex items-center z-10">
+                    <div className="flex flex-col justify-center items-center">
                         <LogoIcon className="2xl:w-[46px] 2xl:h-[46px] xl:w-[46px] xl:h-[42px] lg:w-[46px] lg:h-[36px]" />
                         <p className="text-white text-4xl font-semibold whitespace-nowrap 4xl:text-2xl 3xl:text-xl 2xl:text-lg xl:text-xs lg:text-xs">
                             FUN SCRUT
@@ -32,8 +32,8 @@ const HeaderDesktop: React.FC = () => {
                         </Button>
                     </div>
                     <div className="flex gap-[50px] 4xl:gap-[1.5vw] 3xl:gap-[20px] 2xl:gap-[1.5vw] xl:gap-[1.5vw] lg:gap-[1.5vw]">
-                        <ShoppingCartIcon className="flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px]" />
-                        <ProfileIcon className="flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px]" />
+                        <ShoppingCartIcon className="cursor-pointer flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px]" />
+                        <ProfileIcon className="cursor-pointer flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px]" />
                     </div>
                 </div>
             </header>
--- frontend-react/components/mobi/layout/HeaderMobi/HeaderMobi.tsx
+++ frontend-react/components/mobi/layout/HeaderMobi/HeaderMobi.tsx
@@ -1,41 +1,69 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 
-import { LogoIcon } from '@/components/asssets/icons'
 import HeaderNavigationMobi from './HeaderNavigationMobi'
-import { ShoppingCartIcon, ProfileIcon } from '@/components/asssets/icons'
+import {
+    ShoppingCartIcon,
+    ProfileIcon,
+    LogoIcon,
+    PhoneIcon,
+    BurgerMenuIcon,
+    CrossIcon,
+    TelegramIcon,
+    InstagramIcon,
+    VkIcon,
+} from '@/components/asssets/icons'
 import { Button } from '@/components/ui/button'
 
 const HeaderMobi: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
     return (
         <>
-            <header
-                className="relative flex justify-center pt-[34px] pb-[35px] px-[59px] bg-[rgba(16,16,48,0.7)] border border-red-500 border-solid"
-                style={{
-                    backgroundImage: 'url(/background/colorBackground.png)',
-                    backgroundSize: 'cover',
-                }}
-            >
-                <div className="absolute inset-0 bg-[rgba(16,16,48,0.7)]"></div>
-                <div className="relative w-full max-w-[1800px] justify-between flex items-center z-10 border border-red-500 border-solid">
-                    <div className="flex flex-col justify-center items-center border border-red-500 border-solid">
-                        <LogoIcon className="2xl:w-[46px] 2xl:h-[46px] xl:w-[46px] xl:h-[42px] lg:w-[46px] lg:h-[36px] md:w-[46px] md:h-[36px]" />
-                        <p className="text-white text-4xl font-semibold whitespace-nowrap 4xl:text-2xl 3xl:text-xl 2xl:text-lg xl:text-xs lg:text-xs md:text-xs">
-                            FUN SCRUT
-                        </p>
+            <header className="bg-[#101030] pl-[12px] pr-[8px]">
+                <div className="max-w-[768px] flex items-center justify-between">
+                    <PhoneIcon className="cursor-pointer" />
+                    <div className="flex flex-col justify-center items-center">
+                        <LogoIcon className="w-[34px] h-[34px]" />
+                        <p className="text-white text-xs font-semibold whitespace-nowrap">FUN SCRUT</p>
                     </div>
-                    <div className="flex gap-[32px] 4xl:gap-[2vw] 3xl:gap-[10px] 2xl:gap-[10px] xl:gap-[20px] lg:gap-[10px] md:gap-[10px]">
-                        <Button variant="header_desktop_btn_gradient" size="header_btn">
-                            Заказать звонок
-                        </Button>
-                    </div>
-                    <div className="flex gap-[50px] 4xl:gap-[1.5vw] 3xl:gap-[20px] 2xl:gap-[1.5vw] xl:gap-[1.5vw] lg:gap-[1.5vw] md:gap-[1.5vw]">
-                        <ShoppingCartIcon className="flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px] md:w-[20px] md:h-[20px]" />
-                        <ProfileIcon className="flex-shrink-0 w-[50px] h-[50px] 4xl:w-[40px] 4xl:h-[40px] 3xl:w-[36px] 3xl:h-[36px] 2xl:w-[30px] 2xl:h-[30px] xl:w-[24px] xl:h-[24px] lg:w-[20px] lg:h-[20px] md:w-[20px] md:h-[20px]" />
+                    <div className="flex gap-[17px]">
+                        <ShoppingCartIcon className="cursor-pointer flex-shrink-0 w-[24px] h-[24px]" />
+                        <BurgerMenuIcon
+                            className="cursor-pointer flex-shrink-0 w-[24px] h-[24px]"
+                            onClick={toggleMenu}
+                        />
                     </div>
                 </div>
             </header>
+
+            {isMenuOpen && (
+                <div className="fixed inset-0 bg-[#101030] z-50 flex flex-col items-center p-6 text-white">
+                    <div className="w-full flex justify-end opacity-50 hover:opacity-100 transition-opacity duration-300 cursor-pointer">
+                        <CrossIcon onClick={toggleMenu} />
+                    </div>
+                    <div className="flex gap-[6px] flex-col items-center pt-[8px] pb-[26px] ">
+                        <div className="flex items-center gap-[10px]">
+                            <ProfileIcon className="md:w-[34px] md:h-[34px] sm_xl:w-[34px] sm_xl:h-[34px] sm_l:w-[28px] sm_l:h-[28px] sm:w-[28px] sm:h-[28px]" />
+                            <p className="uppercase font-semibold whitespace-nowrap md:text-5xl sm_xl:text-5xl sm_l:text-4xl sm:text-4xl">
+                                Войти в профиль
+                            </p>
+                        </div>
+                        <div className="w-full h-[2px] border-b border-white-500 border-solid"></div>
+                    </div>
+                    <HeaderNavigationMobi />
+                    <Button variant="select_mobi" size="select_mobi" className="mt-[28px] hover:bg-gradient-mobi-menu">
+                        Заказать звонок
+                    </Button>
+                    <div className="flex gap-[18px] pt-[18px]">
+                        <TelegramIcon className="cursor-pointer" />
+                        <InstagramIcon className="cursor-pointer" />
+                        <VkIcon className="cursor-pointer" />
+                    </div>
+                </div>
+            )}
         </>
     )
 }
