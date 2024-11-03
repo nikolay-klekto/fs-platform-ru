912b12f046edc272a8f26047e1a2a957d0864e87
Daria <scheglova.daria@gmail.com>
2024-11-04 01:11:40 +0300

    add: desktop adaptive styles

--- frontend-react/components/desktop/layout/EventsSectionDesktop/ItemEventsDesktop/ItemEventsDesktop.tsx
+++ frontend-react/components/desktop/layout/EventsSectionDesktop/ItemEventsDesktop/ItemEventsDesktop.tsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Image from 'next/image'
 
 interface EventSectionDesktopProps {
     image: string
     title: string
     date: string
+    onWidthChange: (width: number) => void
 }
 
-const ItemEventsDesktop: React.FC<EventSectionDesktopProps> = ({ image, title, date }) => {
+const ItemEventsDesktop: React.FC<EventSectionDesktopProps> = ({ image, title, date, onWidthChange }) => {
     const [day, month] = date.split(' ')
 
+    const itemRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (itemRef.current) {
+            onWidthChange(itemRef.current.offsetWidth)
+        }
+    }, [])
+
     return (
-        <div className="pb-[100px]">
-            <div className="relative min-h-[454px] min-w-[520px]">
+        <div ref={itemRef} className="pb-[100px] 2xl:pb-[84px] xl:pb-[78px] lg:pb-[70px]">
+            <div className="relative aspect-[4/3] w-[520px] 4xl:w-[421px] 3xl:w-[389px] 2xl:w-[345px] xl:w-[276px] lg:w-[207px]">
                 <Image
                     src={image}
                     fill
                     alt={title}
-                    className="object-cover rounded-[50px] select-none pointer-events-none"
+                    className="object-cover rounded-[50px] 3xl:rounded-[45px] 2xl:rounded-[45px] xl:rounded-[40px] lg:rounded-[35px] select-none pointer-events-none"
                 />
-                <div className="absolute right-[23px] top-[18px] w-[114px] h-[101px] rounded-[23px] bg-white bg-opacity-80 flex flex-col justify-center items-center">
-                    <p className="text-9xl font-semibold text-[#1f203f]">{day}</p>
-                    <p className="text-4xl font-medium text-[#878797]">{month}</p>
+                <div className="absolute right-[5%] top-[4%] aspect-[113/100] w-[114px] 4xl:w-[92px] 3xl:w-[85px] 2xl:w-[75px] xl:w-[60px] lg:w-[60px] rounded-[23px] 2xl:rounded-[20px] xl:rounded-[18px] lg:rounded-[16px] bg-white bg-opacity-80 flex flex-col justify-center items-center">
+                    <p className="text-9xl  4xl:text-7xl 3xl:text-6xl 2xl:text-5xl xl:text-2xl lg:text-sm font-semibold text-[#1f203f]">
+                        {day}
+                    </p>
+                    <p className="text-4xl  4xl:text-2xl 3xl:text-xl 2xl:text-lg xl:text-xs lg:text-xs font-medium text-[#878797]">
+                        {month}
+                    </p>
                 </div>
             </div>
-            <p className="uppercase text-9xl font-medium text-white pt-5 min-h-[64px] max-h-[64px]">{title}</p>
+            <p className="uppercase font-medium text-white pt-5 line-clamp-2 leading-[1.2] max-h-[calc(2*2.5rem)] overflow-hidden text-ellipsis text-9xl 4xl:text-7xl 3xl:text-6xl 2xl:text-5xl xl:text-2xl lg:text-sm">
+                {title}
+            </p>
         </div>
     )
 }
--- frontend-react/components/desktop/shared/TitleDesktop.tsx
+++ frontend-react/components/desktop/shared/TitleDesktop.tsx
@@ -7,13 +7,17 @@ interface PropsTitle {
 
 const TitleDesktop: React.FC<PropsTitle> = ({ title, href }) => {
     return (
-        <div className="relative mb-[80px] px-[47px]">
-            <h2 className="text-[160px] text-white opacity-[0.030]">{title}</h2>
+        <div className="relative inline-block">
+            <h2 className=" px-[47px] text-[160px] 4xl:text-[123px] 3xl:text-[114px] 2xl:text-[96px] xl:text-[72px] lg:text-[50px] m-0 leading-none text-white opacity-[0.030]">
+                {title}
+            </h2>
             <Link
                 href={href}
-                className="absolute bottom-[37px] left-[57px] bg-gradient-desktop hover:bg-gradient-desktop-hover bg-clip-text text-transparent"
+                className="absolute bottom-[7%] left-[1%] px-[47px] bg-gradient-desktop hover:bg-gradient-desktop-hover bg-clip-text text-transparent"
             >
-                <h4 className="text-35xl">{title}</h4>
+                <h4 className="uppercase text-35xl 4xl:text-27xl 3xl:text-25xl 2xl:text-21xl xl:text-16xl lg:text-11xl m-0 leading-none">
+                    {title}
+                </h4>
             </Link>
         </div>
     )
