f7eebfc14d72a5c71423ff1754631cdd8196cac9
Daria <scheglova.daria@gmail.com>
2024-11-04 02:07:00 +0300

    add: mobi adaptive styles

--- frontend-react/components/mobi/layout/EventsSectionMobi/ItemEventsMobi/ItemEventsMobi.tsx
+++ frontend-react/components/mobi/layout/EventsSectionMobi/ItemEventsMobi/ItemEventsMobi.tsx
@@ -13,23 +13,29 @@ const ItemEventsMobi: React.FC<EventSectionMobiProps> = ({ image, title, date, a
 
     return (
         <div className="pb-[32px]">
-            <div className="relative min-h-[294px] min-w-[346px]">
+            <div className="relative aspect-[4/3] w-[295px] md:w-[390px] sm_xl:w-[345px] sm_l:w-[295px] sm:w-[290px]">
                 <Image
                     src={image}
                     fill
                     alt={title}
-                    className="object-cover rounded-[25px] select-none pointer-events-none"
+                    className="object-cover rounded-[25px] select-none pointer-events-none "
                 />
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#232341] rounded-[25px]" />
-                <div className="absolute right-[14px] top-[14px] w-[60px] h-[69px] rounded-[15px] bg-white bg-opacity-80 flex flex-col justify-center items-center">
-                    <p className="text-4xl leading-[22px] font-semibold text-[#1f203f]">{day}</p>
-                    <p className="text-base leading-[15px] font-medium text-[#878797]">{month}</p>
+                <div className="absolute right-[14px] top-[14px] aspect-[87/100] w-[60px] sm_l:w-[55px] sm:w-[55px] rounded-[15px] bg-white bg-opacity-80 flex flex-col justify-center items-center">
+                    <p className="text-4xl md:text-4xl sm_xl:text-3xl sm_l:text-2xl sm:text-xl font-semibold text-[#1f203f]">
+                        {day}
+                    </p>
+                    <p className="text-base md:text-base sm_xl:text-sm sm_l:text-xs sm:text-xs leading-[15px] font-medium text-[#878797]">
+                        {month}
+                    </p>
                 </div>
-                <div className="absolute left-[14px] bottom-[14px] inline-block">
-                    <p className="uppercase text-4xl leading-[22px] pb-[14px] font-semibold text-white min-h-[44px] max-h-[44px] flex items-end">
+                <div className="absolute left-[14px] bottom-[14px] pr-[14px] inline-block">
+                    <p className="uppercase text-4xl md:text-4xl sm_xl:text-3xl sm_l:text-2xl sm:text-xl pb-[14px] font-semibold text-white min-h-[44px] max-h-[44px] flex items-end">
                         {title}
                     </p>
-                    <p className="text-base  font-medium text-[#878797]">{adress}</p>
+                    <p className="text-base md:text-base sm_xl:text-sm sm_l:text-xs sm:text-xs font-medium text-[#878797] max-w-[85%] overflow-hidden whitespace-nowrap text-ellipsis">
+                        {adress}
+                    </p>
                 </div>
             </div>
         </div>
