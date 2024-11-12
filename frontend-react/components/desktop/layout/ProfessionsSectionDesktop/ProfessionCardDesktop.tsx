8f28ac4a5e13091e9b42fc694b7b8405b202f62e
Margarita Bondarenko <152335933+Margo-Bond@users.noreply.github.com>
2024-11-12 15:28:50 +0100

    Ff 34 standardize professions block (#65)
    * FF-34-standardize-professions-block: temporarily save profession section changes
    
    * FIX_FF-27-create_header_main: changes to measurement units, removal of extra margins and paddings, removal of styles for laptop,  and removal of useEffect
    
    FF-34-standardize-professions-block: create components ProfessionsSectionDesktop, ProfessionCardDesktop - for desktop
    
    * FIX-FF-27-create_header_main: fix responsive icon, paddings and margins
    
    * FF-34-standardize-professions-block WIP: adding desktop version of Professions block, replaced profession card with UI card, still in progress
    
    * FF-34-standardize-professions-block: style: update styles for ProfessionCardDesktop to improve responsiveness
    
    * FF-34-standardize-professions-block style: update styles for ProfessionsSectionDesktop to improve responsiveness
    
    * FF-34-standardize-professions-block Add new ProfessionsSectionMobi component and update related styles

---
+++ frontend-react/components/desktop/layout/ProfessionsSectionDesktop/ProfessionCardDesktop.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardFooter } from '@/components/ui/card'
+
+interface ProfessionCardDesktopProps {
+    profession: string
+    image: string
+    price: string
+}
+
+const ProfessionCardDesktop: React.FC<ProfessionCardDesktopProps> = ({ image, profession, price }) => {
+    return (
+        <>
+            <Card
+                className="3xl:px-[20px] hover:shadow_desktop_custom flex cursor-pointer flex-col justify-between rounded-[50px] border-none bg-cover bg-center px-[30px] py-[20px] 2xl:px-[15px]"
+                style={{ backgroundImage: `url(${image})` }}
+            >
+                <CardHeader className="pl-0 pt-3">
+                    <CardTitle className="text30px_desktop w-fit rounded-[50px] bg-white bg-opacity-[70%] px-[20px] py-[10px] font-semibold tracking-normal text-[#101030]">
+                        {profession}
+                    </CardTitle>
+                </CardHeader>
+                <CardFooter className="pb-3 pl-0">
+                    <p className=" text24px_desktop w-fit rounded-[50px] bg-white bg-opacity-[100] px-[15px] py-[5px] font-medium text-[#878797] 2xl:px-[10px]">
+                        от{' '}
+                        <span className="bg-gradient-desktop text36px_desktop bg-clip-text font-medium text-transparent">
+                            {price} BYN/
+                        </span>
+                        неделя
+                    </p>
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
+
+export default ProfessionCardDesktop
--- frontend-react/components/desktop/pageDesktop/HomePageDesktop/HomePageDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/HomePageDesktop/HomePageDesktop.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import HeaderMainDesktop from '../../layout/HeaderDesktop/HeaderMainDesktop'
 import HeaderCardsDesktop from '../../layout/HeaderDesktop/HeaderCardsDesktop'
 import backgroundImageConfig from '@/tailwind-config/backgroundImageConfig'
-
 const HomePageDesktop: React.FC = () => {
     return (
-        <div
-            className="paddings-desktop-custom flex justify-between"
-            style={{ backgroundImage: backgroundImageConfig['header-main-gradient-desktop'] }}
-        >
-            <HeaderMainDesktop />
-            <HeaderCardsDesktop />
-        </div>
+        <>
+            <div
+                className="paddings-desktop-custom flex justify-between"
+                style={{ backgroundImage: backgroundImageConfig['header-main-gradient-desktop'] }}
+            >
+                <HeaderMainDesktop />
+                <HeaderCardsDesktop />
+            </div>
+        </>
     )
 }
 export default HomePageDesktop
