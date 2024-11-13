11be1638b93ee3b276feee2759415c336c5e3cd2
AlsuGai <alsu.gai05@mail.ru>
2024-11-13 13:52:57 +0300

    edit components

--- frontend-react/components/desktop/layout/HeaderDesktop/HeaderCardItemDesktop.tsx
+++ frontend-react/components/desktop/layout/HeaderDesktop/HeaderCardItemDesktop.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { ForwardIcon } from '@/components/assets/icons'
 
@@ -21,64 +20,50 @@ const HeaderCardItemDesktop: React.FC<HeaderCardItemDesktop> = ({
     currency,
     time,
 }) => {
-    const [isHovered, setIsHovered] = useState(false)
-
-    const handleMouseEnter = () => {
-        setIsHovered(true)
-    }
-
-    const handleMouseLeave = () => {
-        setIsHovered(false)
-    }
-
     return (
         <>
             <div
-                className="flex min-h-[260px] w-[426px] flex-1 flex-col justify-between gap-[17px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] md:min-h-[140px] md:max-w-[230px] md:gap-[18px] 3xl:min-h-[198px] 3xl:w-[324px] 2xl:min-h-[188px] 2xl:w-[308px] xl:min-h-[184px] xl:w-[302px] lg:min-h-[160px] lg:w-[262px]"
+                className="min-h-[260px] max-w-[426px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
                 style={{
                     backgroundImage: "url('/background/subtract_desk.svg')",
                     backgroundSize: 'contain, contain',
-                    backgroundPosition: 'center, rigth',
+                    backgroundPosition: 'center, center',
                     backgroundRepeat: 'no-repeat, no-repeat',
                 }}
             >
-                <div className=" grow px-10 md:px-4 lg:px-6 xl:px-7 pt-[36px] 3xl:pt-[26px] 3xl:px-7 2xl:pt-[26px] 2xl:px-5 lg:pt-[22px] ">
-                    <div className="text-7xl font-medium md:text-2xl md:font-light lg:text-3xl xl:text-3xl text-justify leading-[40px] md:leading-[8px] lg:leading-[20px] 3xl:leading-[24px] 3xl:text-4xl 2xl:leading-[24px] 2xl:text-4xl xl:leading-[24px] ">
-                        {textBlack}{' '}
-                        <span
-                            className="bg-gradient-desktop font-bold  md:font-semibold"
+                <div className="flex flex-1 flex-col justify-between gap-[17px]">
+                    <div className="3xl:pt-[26px] 3xl:px-7 grow px-10 pt-[36px] md:px-4 2xl:px-5 2xl:pt-[26px] ">
+                        <div className="3xl:leading-[24px] 3xl:text-4xl text-justify text-7xl font-medium leading-[40px] md:text-2xl md:font-light md:leading-[8px] 2xl:text-4xl 2xl:leading-[24px] ">
+                            {textBlack}{' '}
+                            <span
+                                className="bg-gradient-desktop font-bold  md:font-semibold"
+                                style={{
+                                    WebkitBackgroundClip: 'text',
+                                    WebkitTextFillColor: 'transparent',
+                                }}
+                            >
+                                {textColor}{' '}
+                            </span>
+                            {textBlackBr}
+                        </div>
+                    </div>
+                    <div className="3xl:pl-7 flex grow justify-between pl-10 md:pl-4 xl:pl-7 2xl:pl-5">
+                        <div
+                            className="bg-gradient-desktop text-5xl font-semibold md:text-2xl"
                             style={{
                                 WebkitBackgroundClip: 'text',
                                 WebkitTextFillColor: 'transparent',
                             }}
                         >
-                            {textColor}{' '}
-                        </span>
-                        {textBlackBr}
+                            от{' '}
+                            <span className="text-11xl 3xl:text-6xl font-medium md:text-3xl 2xl:text-6xl">
+                                {price} {currency}/ {time}
+                            </span>
+                        </div>
+                        <Button variant="circle" size="circleDesk" className="3xl:size-[52px] self-end 2xl:size-[52px]">
+                            <ForwardIcon />
+                        </Button>
                     </div>
-                </div>
-                <div className="grow flex justify-between pl-10 md:pl-4 lg:pl-6 xl:pl-7 3xl:pl-7 2xl:pl-5">
-                    <div
-                        className="bg-gradient-desktop text-5xl font-semibold md:text-2xl xl:text-4xl"
-                        style={{
-                            WebkitBackgroundClip: 'text',
-                            WebkitTextFillColor: 'transparent',
-                        }}
-                    >
-                        от{' '}
-                        <span className="text-11xl font-medium md:text-3xl lg:text-4xl xl:text-5xl 3xl:text-6xl 2xl:text-6xl">
-                            {price} {currency}/ {time}
-                        </span>
-                    </div>
-                    <Button
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                        variant="accent_desktop"
-                        size="circleDesk"
-                        className={`${isHovered ? 'button-shadow_right_desktop_custom' : ''} self-end 3xl:size-[52px] 2xl:size-[52px] lg:size-[46px] xl:size-[50px]`}
-                    >
-                        <ForwardIcon className="lg:h-[18px] lg:w-[21px] xl:h-[21px] xl:w-[24px]" />
-                    </Button>
                 </div>
             </div>
         </>
--- frontend-react/components/desktop/pageDesktop/HomePageDesktop/HomePageDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/HomePageDesktop/HomePageDesktop.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import HeaderMainDesktop from '../../layout/HeaderDesktop/HeaderMainDesktop'
 import HeaderCardsDesktop from '../../layout/HeaderDesktop/HeaderCardsDesktop'
 import backgroundImageConfig from '@/tailwind-config/backgroundImageConfig'
+import HowWeWorkDesktop from '../../layout/HowWeWorkDesktop/HowWeWorkDesktop'
 
 const HomePageDesktop: React.FC = () => {
     return (
-        <div
-            className="paddings-desktop-custom flex justify-between"
-            style={{ backgroundImage: backgroundImageConfig['header-main-gradient-desktop'] }}
-        >
-            <HeaderMainDesktop />
-            <HeaderCardsDesktop />
-        </div>
+        <>
+            <div style={{ backgroundImage: backgroundImageConfig['header-main-gradient-desktop'] }}>
+                <div className="paddings-desktop-custom container flex justify-between">
+                    <HeaderMainDesktop />
+                    <HeaderCardsDesktop />
+                </div>
+            </div>
+            <HowWeWorkDesktop />
+        </>
     )
 }
 export default HomePageDesktop
