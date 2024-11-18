6d89e88cf1671149008a97de30c6ac209d43e38d
Margo-Bond <ritabond1991@gmail.com>
2024-11-18 17:53:46 +0100

    FF-38-standardize-howwework-block fix: change height for cards

--- frontend-react/components/desktop/layout/HowWeWorkDesktop/HowWeWorkDesktop.tsx
+++ frontend-react/components/desktop/layout/HowWeWorkDesktop/HowWeWorkDesktop.tsx
@@ -9,11 +9,11 @@ const HowWeWorkDesktop: React.FC = () => {
                 <div className="mb-[122px]">
                     <TitleDesktop title="Как мы работаем" href="#" />
                 </div>
-                <div className="flex flex-row items-center gap-10">
+                <div className="3xl:gap-8 flex flex-row items-center gap-10 2xl:gap-5">
                     {content.map((item) => (
                         <div
                             key={item.id}
-                            className="relative flex min-h-64 w-[414px] flex-col items-center rounded-[22px] bg-[#ffffff1a] px-5 pt-[80px] text-center"
+                            className="relative flex min-h-60 w-[400px] flex-col items-center rounded-[22px] bg-[#ffffff1a] px-5 pt-[80px] text-center 2xl:pt-[60px]"
                         >
                             <div className="3xl:top-[-40px] absolute top-[-50px] rounded-full border-8 border-[#101030] 2xl:top-[-40px]">
                                 <div className="bg-gradient-desktop 3xl:size-[75px] flex size-[85px] items-center justify-center rounded-full bg-[#101030] 2xl:size-[70px]">
