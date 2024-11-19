7bfe5337681ec8bd975c09f2bd75b32c3fd165ee
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-11-19 15:55:01 +0300

    feat: create new page - contacts desktop

---
+++ frontend-react/components/desktop/layout/ContactsDesktop/ContactsDesktop.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ContactsDesktop: React.FC = () => {
+    return (
+        <>
+            <div className="flex pt-52 pb-[297px]border border-red-500">
+                <div className="flex flex-col max-w-[541px] border border-red-500">
+                    <h2 className="text-26xl">Свяжитесь с нами</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ContactsDesktop
