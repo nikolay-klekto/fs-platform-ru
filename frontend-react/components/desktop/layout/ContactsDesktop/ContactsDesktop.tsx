14e7b57f5b455205dd98ca1079f1ddf955e6ec85
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-11-20 12:26:44 +0300

    feat: add social block

--- frontend-react/components/desktop/layout/ContactsDesktop/ContactsDesktop.tsx
+++ frontend-react/components/desktop/layout/ContactsDesktop/ContactsDesktop.tsx
@@ -1,16 +1,60 @@
 import React from 'react'
 
 import { Button } from '@/components/ui/button'
+import { contentContactsDesktop, contentSocialContactsDesktop } from './contentContactsDesktop'
 
 const ContactsDesktop: React.FC = () => {
     return (
         <>
             <div className="flex justify-between pt-52 pb-[297px] border border-red-500">
                 <div className="flex flex-col gap-7 max-w-[541px] border border-red-500">
-                    <h2 className="text-26xl font-semibold">Cвяжитесь с нами</h2>
+                    <h2 className="text-26xl font-semibold uppercase">Cвяжитесь с нами</h2>
                     <Button variant="select_desktop" size="contacts_btn">
                         Хочу в команду
                     </Button>
+                </div>
+                <div className="w-[1021px]">
+                    <div className="flex justify-between border border-red-500">
+                        <div className="flex flex-col">
+                            {contentContactsDesktop.map((item) => (
+                                <div
+                                    key={item.id}
+                                    className={`${item.id !== contentContactsDesktop.length ? 'pb-[60px]' : ''}`}
+                                >
+                                    <p className="pb-[5px] text-7xl font-semibold text-white/50">{item.title}</p>
+                                    {item.href ? (
+                                        <a
+                                            href={item.href}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-15xl font-semibold"
+                                        >
+                                            {item.value}
+                                        </a>
+                                    ) : (
+                                        <p className="text-14xl font-semibold">{item.value}</p>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex flex-col border border-red-500 justify-between pt-2.5">
+                            {contentSocialContactsDesktop.map((item) => (
+                                <div
+                                    key={item.id}
+                                    className="flex items-center gap-5 max-w-[376px] border border-red-500"
+                                >
+                                    <a
+                                        href={item.href}
+                                        className="flex items-center justify-center w-[58px] h-[62px] rounded-full bg-black"
+                                    >
+                                        {item.icon}
+                                    </a>
+                                    <p className="text-7xl font-medium">{item.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="w-full h-[1px] bg-white/50 rounded-full mt-[99px] mb-[63px]"></div>
                 </div>
             </div>
         </>
---
+++ frontend-react/components/desktop/layout/ContactsDesktop/contentContactsDesktop.tsx
@@ -0,0 +1,75 @@
+import { ReactNode } from 'react'
+
+import {
+    InstagramIconDesktop,
+    TelegramIconDesktop,
+    LinkedInIconDesktop,
+    PhoneIconDesktop,
+} from '@/components/assets/icons'
+
+interface Contacts {
+    id: number
+    title: string
+    value: string
+    href?: string
+}
+
+export const contentContactsDesktop: Contacts[] = [
+    {
+        id: 1,
+        title: 'Для клиентов и партнеров',
+        value: 'abcd@mail.com',
+        href: 'mailto:abcd@mail.com',
+    },
+    {
+        id: 2,
+        title: 'Для клиентов и партнеров',
+        value: 'abcd@mail.com',
+        href: 'mailto:abcd@mail.com',
+    },
+    {
+        id: 3,
+        title: 'Для соискателей',
+        value: 'jobs@funscurt.com',
+        href: 'mailto:jobs@funscurt.com',
+    },
+    {
+        id: 4,
+        title: 'Минск',
+        value: 'ул. Петруся Бровки, д.8',
+    },
+]
+
+interface SocialContacts {
+    id: number
+    icon: ReactNode
+    name: string
+    href: string
+}
+
+export const contentSocialContactsDesktop: SocialContacts[] = [
+    {
+        id: 1,
+        icon: <InstagramIconDesktop width={34} height={34} />,
+        name: 'Instagram с мемами',
+        href: '#',
+    },
+    {
+        id: 2,
+        icon: <TelegramIconDesktop width={34} height={30} />,
+        name: 'Telegram канал с полезными статьями',
+        href: '#',
+    },
+    {
+        id: 3,
+        icon: <LinkedInIconDesktop />,
+        name: 'LinkedIn с открытыми вакансиями',
+        href: '#',
+    },
+    {
+        id: 4,
+        icon: <PhoneIconDesktop />,
+        name: '+375 (29) 000 00 00',
+        href: 'tel:+375290000000',
+    },
+]
