6cf0de33b60210d7ae4e1cc0031e8686024e63ad
Margo-Bond <ritabond1991@gmail.com>
2024-12-07 22:16:40 +0100

    FF-52_move_mobile_button fix: fix button position in HeaderMainMobi and add padding for parent container

--- frontend-react/components/mobi/layout/HeaderMobi/HeaderMainMobi.tsx
+++ frontend-react/components/mobi/layout/HeaderMobi/HeaderMainMobi.tsx
@@ -20,16 +20,16 @@ const HeaderMainMobi: React.FC = () => {
                     }}
                 >
                     <HeaderMobi disableBackground={true} />
-                    <div>
+                    <div className="md:px-[15px]">
                         <div className="min-w-[70%] max-w-[90%]">
-                            <p className="sm_xl:text-[2rem] sm_l:text-[1.875rem]  w-full break-words pb-[10px] uppercase leading-[40px] text-white sm:text-[1.75rem] md:text-[2.125rem]">
+                            <h1 className="sm_xl:text-[2rem] sm_l:text-[1.875rem] sm_s:text-[1.875rem] w-full break-words pb-[10px] uppercase leading-[40px] text-white sm:text-[1.75rem] md:text-[2.125rem]">
                                 Помогаем пройти стажировку
-                            </p>
+                            </h1>
                             <p className="w-full break-words pb-[40px] text-[0.875rem] font-medium text-white">
                                 В интересующей профессии и компании, независимо от наличия опыта и навыков
                             </p>
                         </div>
-                        <div className="mx-auto flex w-fit items-center justify-center">
+                        <div className="mx-auto md:ml-0 flex w-fit items-center justify-center">
                             <Link href="/professions">
                                 <Button variant="accent_mobi" size="wide_mobi">
                                     <span className="bg-gradient-desktop sm_s:p-[14px] bg-clip-text px-[20px] text-[1.125rem] font-semibold text-transparent sm:px-[10px]">
