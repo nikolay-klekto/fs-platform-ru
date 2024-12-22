0d203874e9716a9a3f4e6cd32fde625a621b3e2b
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-12-22 15:30:25 +0300

    feat: add textAbout style, add clamp padding

--- frontend-react/components/desktop/layout/ProfessionModalDesktop/ProfessionModalDesktop.tsx
+++ frontend-react/components/desktop/layout/ProfessionModalDesktop/ProfessionModalDesktop.tsx
@@ -15,15 +15,28 @@ const ProfessionModalDesktop: React.FC<ProfessionModalDesktopProps> = ({ closeMo
     console.log('Номер id: ', { professionId })
 
     return (
-        <Modal show={true} onClose={closeModal} size="large" showCloseButton={false}>
-            <div className="flex flex-col pt-[50px] pl-[88px] pb-[51px] border border-red-500">
+        <Modal
+            show={true}
+            onClose={closeModal}
+            size="large-l"
+            showCloseButton={false}
+            className="custom_modal_large_padding_desktop"
+        >
+            <div className="flex flex-col custom_modal_large_padding_content_desktop border border-red-500">
                 <button onClick={closeModal} className="absolute top-[23px] right-[36px] border border-red-500">
                     <X size={41} className="color-[#878797] opacity-50 hover:opacity-80"></X>
                 </button>
-                <h2 className="mb-7 4xl:mb-6 3xl:mb-5 2xl:mb-4 text46px_desktop font-medium text-gradient_desktop_custom uppercase">
+                <h2 className="mb-[5px] text46px_desktop font-medium text-gradient_desktop_custom uppercase border border-red-500">
                     {profession}
                 </h2>
-                <p>{contentProfessionAbout.map((item) => item.text)}</p>
+                {contentProfessionAbout.map((item) => (
+                    <p
+                        key={item.id}
+                        className="max-w-[1190px] custom_modal_large_margin_content_desktop text18px_modal_text_desktop border border-red-500"
+                    >
+                        {item.text}
+                    </p>
+                ))}
             </div>
         </Modal>
     )
--- frontend-react/components/ui/modal.tsx
+++ frontend-react/components/ui/modal.tsx
@@ -6,11 +6,19 @@ interface ModalProps {
     children: ReactNode
     show: boolean
     onClose: () => void
-    size?: 'small' | 'medium' | 'large'
+    size?: 'small' | 'medium' | 'large' | 'large-l'
     showCloseButton?: boolean
+    className?: string
 }
 
-const Modal: React.FC<ModalProps> = ({ children, show, onClose, size = 'medium', showCloseButton = true }) => {
+const Modal: React.FC<ModalProps> = ({
+    children,
+    show,
+    onClose,
+    size = 'medium',
+    showCloseButton = true,
+    className,
+}) => {
     if (!show) return null
 
     const getSizeClass = () => {
@@ -19,6 +27,8 @@ const Modal: React.FC<ModalProps> = ({ children, show, onClose, size = 'medium',
                 return 'max-w-xs'
             case 'large':
                 return 'max-w-4xl'
+            case 'large-l':
+                return 'max-w-[1366px]'
             case 'medium':
             default:
                 return 'max-w-lg'
@@ -26,7 +36,10 @@ const Modal: React.FC<ModalProps> = ({ children, show, onClose, size = 'medium',
     }
 
     return ReactDOM.createPortal(
-        <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50" onClick={onClose}>
+        <div
+            className={`fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50 ${className}`}
+            onClick={onClose}
+        >
             <div
                 className={`relative bg-[#101030] rounded-[50px] w-full ${getSizeClass()} text-white`}
                 onClick={(e) => e.stopPropagation()}
