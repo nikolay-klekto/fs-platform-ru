25f4fcf4a3b0e0ec91133e5df7bd8d44e340aac2
solodolya <oa.solod@gmail.com>
2025-02-04 23:47:27 +1000

    FF-80-feat: fix mobi tooltip

--- frontend-react/components/ui/tooltip.tsx
+++ frontend-react/components/ui/tooltip.tsx
@@ -19,7 +19,7 @@ const PopoverTrigger = PopoverPrimitive.Trigger
 const PopoverContent = React.forwardRef<
     React.ElementRef<typeof PopoverPrimitive.Content>,
     React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = 'start', side= 'top', sideOffset = 4, alignOffset= 50, avoidCollisions, collisionBoundary, ...props }, ref) => (
+>(({ className, align = 'start', side= 'top', sideOffset = 4, alignOffset= 50, avoidCollisions, collisionBoundary, arrowPadding, collisionPadding,...props }, ref) => (
     <PopoverPrimitive.Portal>
         <PopoverPrimitive.Content
             ref={ref}
@@ -27,10 +27,12 @@ const PopoverContent = React.forwardRef<
             side={side}
             sideOffset={sideOffset}
             alignOffset= {alignOffset}
+            arrowPadding={arrowPadding}
             avoidCollisions={avoidCollisions}
+            collisionPadding={collisionPadding}
             collisionBoundary={collisionBoundary}
             className={cn(
-            'z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+            'z-50 w-72 rounded-md bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
             className
             )}
             {...props}
@@ -50,19 +52,22 @@ export const HelpTooltipDesktop: React.FC<HelpTooltipProps> = ({ message }) => (
             </button>
         </PopoverTrigger>
         <PopoverContent
-            className="min-w-[385px] 3xl:min-w-[345px] 2xl:min-w-[308px] min-h-[96px] m-0 p-2 bg-tooltip rounded-[25px] shadow-none border-none"
+            className="min-w-[385px] 3xl:min-w-[345px] 2xl:min-w-[308px] min-h-[96px] m-0 p-2 bg-tooltip rounded-[25px] shadow-none"
             sideOffset={3}
             side="top"
             align="end"
             alignOffset={-20}
+            arrowPadding={3}
+            avoidCollisions={false}
+            collisionPadding={{ top: 20, left: 20 }}
         >
-            <div className="p-3 pt-1">
+            <div className="p-2 pt-1">
                 <p className="text-[15px] align-baseline font-medium 2xl:font-light text-white leading-[19px] text-mauve12">
                     {message}
                 </p>
             </div>
             <PopoverArrow
-                className="h-2 tooltip-arrow"
+                className="h-2 tooltip-arrow border-none"
             />
         </PopoverContent>
     </Popover>
@@ -76,13 +81,16 @@ export const HelpTooltipMobi: React.FC<HelpTooltipProps> = ({ message }) => (
           </button>
       </PopoverTrigger>
       <PopoverContent
-        className="w-[346px] min-h-[96px] m-0 p-2 bg-tooltip rounded-[25px] shadow-none border-none"
-        sideOffset={5}
+        className="min-w-[345px] sm_xl:max-w-[425px] sm_l:min-w-[332px] sm:max-w-[290px] sm_s:min-w-[290px] min-h-[96px] m-auto p-1.5 bg-tooltip rounded-[25px] shadow-none border-none will-change-auto"
+        sideOffset={3}
         side="top"
         align="end"
         alignOffset={-20}
+        arrowPadding={3}
+        avoidCollisions={false}
+        collisionPadding={{ top: 20, left: 20 }}
       >
-          <div className="p-2 pt-1">
+          <div className="p-2">
               <p className="text-[15px] align-baseline font-medium text-white leading-[19px] text-mauve12">
                   {message}
               </p>
