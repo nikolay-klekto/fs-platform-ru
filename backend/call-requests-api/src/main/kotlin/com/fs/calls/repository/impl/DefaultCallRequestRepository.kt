d3cadc11a36a4206855fa1477e8d88cbd5f12946
nikolay-klekto <funScrut@gmail.com>
2025-03-17 21:00:06 +0300

    fix: dependencies [deploy-back]

--- backend/call-requests-api/src/main/kotlin/com/fs/calls/repository/impl/DefaultCallRequestRepository.kt
+++ backend/call-requests-api/src/main/kotlin/com/fs/calls/repository/impl/DefaultCallRequestRepository.kt
@@ -1,12 +1,12 @@
 package com.fs.calls.repository.impl
 
-import com.fs.calls.jooq.tables.pojos.CallRequests
 import com.fs.calls.repository.CallRequestRepository
-import org.jooq.DSLContext
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 
 @Repository
 class DefaultCallRequestRepository(
-    databaseClient: DatabaseClient
-): CallRequestRepository(databaseClient)
+    databaseClient: DatabaseClient,
+    rabbitTemplate: RabbitTemplate
+) : CallRequestRepository(databaseClient, rabbitTemplate)
