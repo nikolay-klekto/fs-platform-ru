20a10212863454cb3e99f7b287bb19633baac2bb
TaisNch <tais.aliexpress@gmail.com>
2025-03-21 19:04:09 +0100

    check on refresh token

--- frontend-react/lib/apolloClient.ts
+++ frontend-react/lib/apolloClient.ts
@@ -1,14 +1,41 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
+import { onError } from '@apollo/client/link/error';
+import Cookies from 'js-cookie';
+import router from 'next/router';
 
 const GRAPHQL_ENDPOINT = 'http://45.135.234.61:8282/graphql'
 
-export function createApolloClient() {
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        console.error(`[GraphQL error]: ${err.message}`);
+        if (
+          err.extensions?.code === 'UNAUTHENTICATED' ||
+          err.message.toLowerCase().includes('токен') ||
+          err.message.toLowerCase().includes('unauthorized')
+        ) {
+          console.warn('Ошибка авторизации, удаляем токены');
+          Cookies.remove('refreshToken');
+          if (typeof window !== 'undefined') {
+            router.push('/login'); 
+          }
+        }
+      }
+    }
+     if (networkError) {
+      console.error(`[Network error]: ${networkError}`);
+    }
+  });
+
+  export function createApolloClient() {
+    const httpLink = new HttpLink({
+      uri: GRAPHQL_ENDPOINT,
+      credentials: 'same-origin', 
+    });
+    const link = from([errorLink, httpLink]); 
     return new ApolloClient({
-        ssrMode: typeof window === 'undefined',
-        link: new HttpLink({
-            uri: GRAPHQL_ENDPOINT,
-            credentials: 'same-origin',
-        }),
-        cache: new InMemoryCache(),
-    })
-}
+      ssrMode: typeof window === 'undefined',
+      link,
+      cache: new InMemoryCache(),
+    });
+  }
