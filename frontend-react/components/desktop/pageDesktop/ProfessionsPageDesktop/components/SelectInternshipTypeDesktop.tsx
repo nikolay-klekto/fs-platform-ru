70b01a25dc8e6e0d28b7288fa17f7d2141580c51
TaisNch <tais.aliexpress@gmail.com>
2025-04-17 16:32:31 +0200

    fix btn styles

--- frontend-react/components/desktop/pageDesktop/ProfessionsPageDesktop/components/SelectInternshipTypeDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/ProfessionsPageDesktop/components/SelectInternshipTypeDesktop.tsx
@@ -14,11 +14,7 @@ interface ISelectOption {
     label: string
 }
 
-const SelectInternshipTypeDesktop = ({
-    onCategoryChange,
-}: {
-    onCategoryChange: (categories: string[]) => void
-}) => {
+const SelectInternshipTypeDesktop = ({ onCategoryChange }: { onCategoryChange: (categories: string[]) => void }) => {
     const [isOpen, setIsOpen] = useState(false)
     const [selectedOptions, setSelectedOptions] = useState<string[]>([])
     const selectRef = useRef<HTMLDivElement>(null)
@@ -75,7 +71,7 @@ const SelectInternshipTypeDesktop = ({
                         background: 'linear-gradient(90deg, #8333f3, #5f4af3, #3b51a8)',
                     }}
                 >
-                    <div className="flex flex-col gap-1 rounded-[42px] bg-[#1F203F] p-3">
+                    <div className="flex flex-col gap-1 rounded-[42px] bg-[rgb(31,32,63)] p-3">
                         {options.map((option) => (
                             <SelectItem
                                 key={option.value}
