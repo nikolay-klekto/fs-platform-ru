ef7d0ce4b28f4e24f37da9e24a43c7d8acaffb90
MalonsoFrey <miss.dunaewa201141@gmail.com>
2025-05-19 17:15:28 +0400

    container style fixed / local data and components added

---
+++ frontend-react/components/desktop/pageDesktop/HomePageDesktop/components/CompaniesSectionDesktop/data/content.tsx
@@ -0,0 +1,143 @@
+export interface IInternshipCompanies {
+    id: number
+    image: {
+        src: string
+        alt: string
+        width: number
+        height: number
+    }
+    price: number
+}
+
+export const contentInternshipCompaniesDesktop: IInternshipCompanies[] = [
+    {
+        id: 1,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 10,
+    },
+    {
+        id: 2,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 20,
+    },
+    {
+        id: 3,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 30,
+    },
+    {
+        id: 4,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 40,
+    },
+    {
+        id: 5,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 50,
+    },
+    {
+        id: 6,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 60,
+    },
+    {
+        id: 7,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 70,
+    },
+    {
+        id: 8,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 80,
+    },
+    {
+        id: 9,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 90,
+    },
+    {
+        id: 10,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 100,
+    },
+    {
+        id: 11,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 110,
+    },
+    {
+        id: 12,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 120,
+    },
+    {
+        id: 13,
+        image: {
+            src: '/images/epamLogo.png',
+            alt: 'company-logo',
+            width: 104,
+            height: 40,
+        },
+        price: 130,
+    },
+]
