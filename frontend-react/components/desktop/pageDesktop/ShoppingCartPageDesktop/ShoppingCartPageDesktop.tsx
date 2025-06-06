7fbe6ec6a5c01069a7285c8deab6ccd921589a6a
MalonsoFrey <miss.dunaewa201141@gmail.com>
2025-06-06 16:01:54 +0400

    styles fixed for screens bigger than 4xl

--- frontend-react/components/desktop/pageDesktop/ShoppingCartPageDesktop/ShoppingCartPageDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/ShoppingCartPageDesktop/ShoppingCartPageDesktop.tsx
@@ -10,8 +10,8 @@ const ShoppingCartPageDesktop: React.FC = () => {
     return (
         <>
             <HeaderDesktop />
-            <main className="bg-[#101030] text-white">
-                <div className="relative min-h-screen overflow-hidden p-[76px_212px_200px_212px] 2xl:p-[60px_100px_100px_100px] 3xl:p-[76px_130px_150px_130px]">
+            <main className="flex justify-center overflow-hidden bg-[#101030] text-white">
+                <div className="relative min-h-screen max-w-[1920px] overflow-x-visible p-[76px_212px_200px_212px] 2xl:p-[60px_100px_100px_100px] 3xl:p-[76px_130px_150px_130px]">
                     <div className="radial-gradient_desktop absolute left-[-400px] top-[-330px] z-0"></div>
                     <div className="relative z-10 flex w-full flex-col items-center justify-center gap-[5%] py-[80px]">
                         <div className="flex flex-col items-center justify-center gap-10">
