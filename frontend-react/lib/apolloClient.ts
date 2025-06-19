5049cd5354c4b5ca832190c5abccdb9e353fd1bb
LeonovEgorF <egor1908608@gmail.com>
2025-06-19 15:20:49 +0300

    test

--- frontend-react/lib/apolloClient.ts
+++ frontend-react/lib/apolloClient.ts
@@ -1,17 +1,16 @@
 // import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
 // import { createErrorLink, createAuthErrorLink } from "./errorLink";
 
-
 // const GRAPHQL_ENDPOINT = 'http://45.135.234.61:8282/graphql'
 
 // export function createApolloClient() {
 //     const httpLink = new HttpLink({
 //       uri: GRAPHQL_ENDPOINT,
-//       credentials: 'same-origin', 
+//       credentials: 'same-origin',
 //     });
-//     const errorLink = createErrorLink();  
-//     const authErrorLink = createAuthErrorLink(); 
-//     const link = from([authErrorLink, errorLink, httpLink]); 
+//     const errorLink = createErrorLink();
+//     const authErrorLink = createAuthErrorLink();
+//     const link = from([authErrorLink, errorLink, httpLink]);
 //     return new ApolloClient({
 //       ssrMode: typeof window === 'undefined',
 //       link,
@@ -19,48 +18,89 @@
 //     });
 //   }
 
-import { ApolloClient, from, HttpLink, InMemoryCache,  split } from '@apollo/client';
-import { createErrorLink, createAuthErrorLink } from "./errorLink";
-import { getMainDefinition } from '@apollo/client/utilities';
-import { OperationDefinitionNode } from 'graphql';
+import { ApolloClient, ApolloLink, from, HttpLink, InMemoryCache, split } from '@apollo/client'
+import { createErrorLink, createAuthErrorLink } from './errorLink'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { OperationDefinitionNode } from 'graphql'
 
+const AUTH_ENDPOINT = 'http://45.135.234.61:8282/graphql'
+const CARDS_ENDPOINT = 'http://45.135.234.61:8183/graphql'
+// export function createApolloClient() {
+//     const authHttpLink = new HttpLink({
+//         uri: AUTH_ENDPOINT,
+//         credentials: 'same-origin',
+//     })
+//
+//     const cardsHttpLink = new HttpLink({
+//         uri: CARDS_ENDPOINT,
+//         credentials: 'same-origin',
+//     })
+//
+//     const errorLink = createErrorLink()
+//     const authErrorLink = createAuthErrorLink()
+//
+//     const routingLink = split(
+//         ({ query }) => {
+//             const definition = getMainDefinition(query)
+//             if (
+//                 definition.kind === 'OperationDefinition' &&
+//                 (definition as OperationDefinitionNode).name?.value?.startsWith('getCards')
+//             ) {
+//                 return true
+//             }
+//             return false
+//         },
+//         cardsHttpLink,
+//         authHttpLink,
+//     )
+//     const link = from([authErrorLink, errorLink, routingLink])
+//     return new ApolloClient({
+//         ssrMode: typeof window === 'undefined',
+//         link,
+//         cache: new InMemoryCache(),
+//     })
+// }
 
-const AUTH_ENDPOINT = 'http://45.135.234.61:8282/graphql';
-const CARDS_ENDPOINT = 'http://45.135.234.61:8183/graphql';
 export function createApolloClient() {
     const authHttpLink = new HttpLink({
-    uri: AUTH_ENDPOINT,
-    credentials: 'same-origin',
-  });
+        uri: AUTH_ENDPOINT,
+        credentials: 'same-origin',
+    })
 
-   const cardsHttpLink = new HttpLink({
-    uri: CARDS_ENDPOINT,
-    credentials: 'same-origin',
-  });
+    const cardsHttpLink = new HttpLink({
+        uri: CARDS_ENDPOINT,
+        credentials: 'same-origin',
+    })
 
-  const errorLink = createErrorLink();
-  const authErrorLink = createAuthErrorLink();
+    const errorLink = createErrorLink()
+    const authErrorLink = createAuthErrorLink()
 
-
-  const routingLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    if (
-      definition.kind === 'OperationDefinition' &&
-      (definition as OperationDefinitionNode).name?.value?.startsWith('getCards')
-    ) {
-      return true;
+    // функция маршрутизации по имени операции
+    function routeByOperationName(name: string) {
+        switch (true) {
+            case name.startsWith('getCards'):
+                return cardsHttpLink
+            default:
+                return authHttpLink
+        }
     }
-    return false;
-  },
-  cardsHttpLink,
-  authHttpLink
-);
-const link = from([authErrorLink, errorLink, routingLink]);
-  return new ApolloClient({
-    ssrMode: typeof window === 'undefined',
-    link,
-    cache: new InMemoryCache(),
-  });
-}
 
+    const routingLink = new ApolloLink((operation, forward) => {
+        const definition = getMainDefinition(operation.query)
+
+        if (definition.kind === 'OperationDefinition' && definition.name?.value) {
+            const selectedLink = routeByOperationName(definition.name.value)
+            return selectedLink.request(operation, forward)
+        }
+
+        return authHttpLink.request(operation, forward)
+    })
+
+    const link = from([authErrorLink, errorLink, routingLink])
+
+    return new ApolloClient({
+        ssrMode: typeof window === 'undefined',
+        link,
+        cache: new InMemoryCache(),
+    })
+}
