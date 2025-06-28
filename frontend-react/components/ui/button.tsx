a71d288e4dd412b38aad6d5dbde0a6ebf60d43c1
Jenikafacet <scissors.cross@gmail.com>
2025-06-28 20:37:21 +0300

    FF-206-feat: fix review comments

--- frontend-react/components/ui/button.tsx
+++ frontend-react/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
                     'button-border-desktop border-2 3xl:text-4xl rounded-[50px] text-[20px] font-semibold text-white  2xl:text-3xl',
                 select_mobi: 'button-border-mobi rounded-[50px] border-2 border-transparent bg-[#101030] text-white',
                 send_btn_mobi:
-                    'button-border-mobi border-2 rounded-[50px] md:text-[15px] sm_xl:text-[14px] sm_l:text-[14px] sm_s:text-[13px] sm:text-[12px] font-semibold text-white',
+                    'button-border-mobi border-2 rounded-[50px] text-[13px] md:text-[15px] lg:text-[17px] xl:text-[17px] 2xl:text-[17px] font-semibold text-white',
                 accent_desktop: 'flex items-center justify-center rounded-[50px] bg-white',
                 accent_mobi: 'flex items-center rounded-[50px] bg-white',
                 registration_desktop:
@@ -71,7 +71,7 @@ const buttonVariants = cva(
                 send_btn_desktop: 'h-[64px] w-[272px] 2xl:w-[200px]',
                 cookie_btn_desktop: '3xl:w-[250px] h-[64px] w-[237px]',
                 cookie_btn_mobi:
-                    'md:h-[47px] md:w-[205px] sm_xl:h-[47px] sm_xl:w-[195px] sm_l:h-[40px] sm_l:w-[195px] sm_s:h-[40px] sm_s:w-full sm:h-[40px] sm:w-full h-[64px] w-[237px] sm:text-wrap',
+                    'h-[47px] w-[195px] md:w-[205px] lg:h-[64px] lg:w-[237px] xl:h-[64px] xl:w-[237px] 2xl:h-[64px] 2xl:w-[237px] text-wrap',
                 circle_btn_gradient_desktop: 'right-0 top-1/2 size-[60px] -translate-y-1/2 rounded-full',
                 circle_btn_mobi: 'h-0 px-[11px] py-0',
                 gradient_btn_desktop: '3xl:w-[250px] h-[64px] w-[272px] 2xl:w-[250px]',
--- frontend-react/modals/ModalsMobi/ModalCookieMobi.tsx
+++ frontend-react/modals/ModalsMobi/ModalCookieMobi.tsx
@@ -21,32 +21,23 @@ export default function ModalCookieMobi({ onClose }: IModalCookieMobi) {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogPortal>
-                <DialogPrimitive.Content className="fixed bottom-5 right-5 sm_xl:right-[15px] sm_l:right-[15px] sm_s:right-[15px] sm:right-0 z-50 box-border flex md:h-[195px] sm_xl:h-fit sm_l:h-fit sm_s:h-fit sm:h-fit h-[204px] md:max-w-[90%] sm_xl:max-w-[93%] sm_l:max-w-[92%] sm_s:max-w-[92%] sm:w-full w-[596px] flex-col justify-between overflow-hidden rounded-[50px] border border-transparent bg-[#101030E6] px-6 py-4 shadow-lg drop-shadow-2xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-right-4 data-[state=closed]:slide-out-to-bottom-4 data-[state=open]:slide-in-from-right-4 data-[state=open]:slide-in-from-bottom-4 sm:rounded-lg">
-                    <div className="mb-1 mt-4 break-words md:text-[15px] sm_xl:text-[14px] sm_l:text-[14px] sm_l:text-[14px] sm_s:text-[13px] sm:text-[12px] text-[17.41px] font-medium leading-snug text-white">
+                <DialogPrimitive.Content className="modal-position-cookie-mobi modal-size-cookie-mobi box-border flex flex-col justify-between overflow-hidden rounded-[50px] border border-transparent bg-[#101030E6] px-6 py-4 shadow-lg drop-shadow-2xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-right-4 data-[state=closed]:slide-out-to-bottom-4 data-[state=open]:slide-in-from-right-4 data-[state=open]:slide-in-from-bottom-4 sm:rounded-lg">
+                    <div className="text13px_cookie_mobi mb-1 mt-4 break-words font-medium leading-snug text-white">
                         Разрешите нам использовать cookie-файлы, чтобы при вашем следующем визите не вводить пароль
                         повторно
-                        <Link
-                            href="#"
-                            className="my-2 mt-1 block md:text-[15px] sm_xl:text-[14px] sm_l:text-[14px] sm_s:text-[13px] sm:text-[12px] text-[17px] text-[#878797]"
-                        >
+                        <Link href="#" className="text13px_cookie_mobi my-2 mt-1 block text-[#878797] underline">
                             Подробнее...
                         </Link>
                     </div>
 
-                    <div className="mr-4 sm_xl:mr-0 sm_l:mr-0 sm_s:mr-0 sm:mr-0 flex sm_xl:flex-col sm_l:flex-col sm_s:flex-col sm:flex-col items-center justify-end gap-[50px] md:gap-[25px] sm_xl:gap-[15px] sm_l:gap-[15px] sm_s:gap-[15px] sm:gap-[15px]">
+                    <div className="md:mr-4 lg:mr-4 xl:mr-4 2xl:mr-4 flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row items-center justify-end gap-[15px] md:gap-[25px] lg:gap-[50px] xl:gap-[50px] 2xl:gap-[50px]">
                         <button
-                            className="md:text-[15px] sm_xl:text-[14px] sm_l:text-[14px] sm_s:text-[13px] sm:text-[12px] text-[17px] font-medium text-[#878797] underline hover:text-[#FFFFFF] focus:outline-none"
+                            className="text13px_cookie_mobi font-medium underline text-[#FFFFFF] focus:outline-none"
                             onClick={handleClose}
                         >
                             Отклонить
                         </button>
-
-                        <Button
-                            variant={'send_btn_mobi'}
-                            size={'cookie_btn_mobi'}
-                            className="text-[17px]"
-                            onClick={handleClose}
-                        >
+                        <Button variant={'send_btn_mobi'} size={'cookie_btn_mobi'} onClick={handleClose}>
                             Облегчить мне задачу
                         </Button>
                     </div>
