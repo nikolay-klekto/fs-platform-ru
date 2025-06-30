850cb3051581297f1b2cdf52ccef2a0128b680c6
TaisNch <tais.aliexpress@gmail.com>
2025-07-01 00:15:57 +0200

    styles and content fix

--- frontend-react/components/desktop/pageDesktop/ProfessionPageDesktop/components/InternshipCompanyDesktop/ItemInternshipCompaniesDesktop/ItemCompaniesDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/ProfessionPageDesktop/components/InternshipCompanyDesktop/ItemInternshipCompaniesDesktop/ItemCompaniesDesktop.tsx
@@ -7,8 +7,6 @@ interface IItemCompaniesDesktop {
     image: {
         src: string
         alt: string
-        width: number
-        height: number
     }
     industry: string
     name: string
@@ -35,22 +33,20 @@ const ItemCompaniesDesktop: React.FC<IItemCompaniesDesktop> = ({ image, name, in
     }, [onWidthChange])
 
     return (
-        <div ref={itemRef} className="relative flex h-auto max-w-[328px] shrink-0 rounded-[20px] text-center ">
-            <Image
-                src={image.src}
-                alt={image.alt}
-                width={image.width}
-                height={image.height}
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                className="rounded-[20px] opacity-60"
-            />
+        <div
+            ref={itemRef}
+            className="relative flex h-auto w-[clamp(280px,_20vw,_328px)] shrink-0 cursor-pointer rounded-[60px] text-center"
+        >
+            <div className="relative aspect-[4/5] w-full">
+                <Image src={image.src} alt={image.alt} fill className="rounded-[20px] opacity-60" />
+            </div>
             <div className="absolute inset-0 flex size-full flex-col items-start justify-between pl-[20px]">
-                <div className="mt-[21px] text-[21px] font-semibold text-white">{industry}</div>
+                <div className="mt-[21px] text20px_desktop font-semibold text-white">{industry}</div>
                 <div className="flex flex-col items-start">
-                    <div className="pl-[10px] text-[30px] font-semibold text-white">{name}</div>
-                    <p className="mb-[23px] w-full rounded-[50px] bg-white pl-[10px] pr-[42px]  text-[24px] font-medium leading-[40px] tracking-normal text-[#878797]">
+                    <div className="pl-[10px] text30px_desktop font-semibold text-white">{name}</div>
+                    <p className="mb-[23px] w-full rounded-[50px] bg-white pl-[10px] pr-[42px]  text24px_desktop font-medium leading-[40px] tracking-normal text-[#878797]">
                         от{' '}
-                        <span className="text-[[28px] bg-gradient-desktop bg-clip-text text-transparent ">
+                        <span className="bg-gradient-desktop bg-clip-text text28px_desktop text-transparent ">
                             {price} BYN/
                         </span>
                         неделя
--- frontend-react/components/desktop/pageDesktop/ProfessionPageDesktop/components/InternshipCompanyDesktop/contentInternshipCompaniesDesktop/content.tsx
+++ frontend-react/components/desktop/pageDesktop/ProfessionPageDesktop/components/InternshipCompanyDesktop/contentInternshipCompaniesDesktop/content.tsx
@@ -3,8 +3,6 @@ interface IInternshipNames {
     image: {
         src: string
         alt: string
-        width: number
-        height: number
     }
     price: number
     industry: string
@@ -16,8 +14,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 30,
         id: 1,
@@ -28,8 +24,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 40,
         id: 2,
@@ -40,8 +34,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 35,
         id: 3,
@@ -52,8 +44,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 50,
         id: 4,
@@ -64,8 +54,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 45,
         id: 5,
@@ -76,8 +64,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 55,
         id: 6,
@@ -89,8 +75,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 60,
         id: 7,
@@ -101,8 +85,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 50,
         id: 8,
@@ -113,8 +95,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 40,
         id: 9,
@@ -125,8 +105,6 @@ export const contentInternshipNamesDesktop: IInternshipNames[] = [
         image: {
             src: '/images/epam.png',
             alt: 'company-logo',
-            width: 320,
-            height: 400,
         },
         price: 35,
         id: 10,
