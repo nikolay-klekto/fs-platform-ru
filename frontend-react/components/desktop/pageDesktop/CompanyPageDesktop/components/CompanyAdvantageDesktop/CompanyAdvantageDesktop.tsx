11a38ef31fc64478f29c66317b80b334c38dc88e
TaisNch <tais.aliexpress@gmail.com>
2025-07-03 14:38:07 +0200

    fix build

--- frontend-react/components/desktop/pageDesktop/CompanyPageDesktop/components/CompanyAdvantageDesktop/CompanyAdvantageDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/CompanyPageDesktop/components/CompanyAdvantageDesktop/CompanyAdvantageDesktop.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useState, useLayoutEffect, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { content } from './contentCompanyAdvantageDesktop/content'
 import ItemCompanyAdvantageDesktop from './ItemCompanyAdvantageDesktop/ItemCompanyAdvantageDesktop'
 
