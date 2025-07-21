605f69ae0f19a99560617b30ceec10130ffd5cfc
MalonsoFrey <miss.dunaewa201141@gmail.com>
2025-07-21 11:43:20 +0400

    style: apply review feedback

--- frontend-react/components/mobi/pageMobi/HomePageMobi/components/CompaniesSectionMobi/CompaniesSectionMobi.tsx
+++ frontend-react/components/mobi/pageMobi/HomePageMobi/components/CompaniesSectionMobi/CompaniesSectionMobi.tsx
@@ -11,7 +11,7 @@ const CompaniesSectionMobi: React.FC = () => {
     return (
         <section className="flex max-w-full flex-col gap-4 px-[15px] pb-[60px] sm:gap-[15px] sm:pt-[15px]">
             <TitleMobi title="Компании" href="/companies" />
-            <div className="scrollbar_custom mx-auto mb-[20px] flex w-full justify-center gap-2 overflow-y-hidden overflow-x-scroll py-[20px]">
+            <div className="scrollbar_custom mx-auto mb-[20px] flex w-full justify-left gap-2 overflow-y-hidden overflow-x-scroll py-[20px]">
                 {companies.slice(0, 10).map((item) => (
                     <div
                         key={item.id}
