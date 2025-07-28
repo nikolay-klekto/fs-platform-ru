2211a9aa167c147cac49185d68a17567d4ac92e3
KoraDraw <korinachernysheva@mail.ru>
2025-07-28 15:12:38 +0300

    add two variables

--- frontend-react/components/desktop/shared/TitleDesktop.tsx
+++ frontend-react/components/desktop/shared/TitleDesktop.tsx
@@ -7,52 +7,36 @@ interface ITitle {
 }
 
 const TitleDesktop: React.FC<ITitle> = ({ title, href }) => {
-    if (href) {
-        return (
-            <div className="relative">
-                <h2
-                    className="4xl:text-[7.5rem] 3xl:text-[7rem] absolute bottom-0 z-0 text-[160px] font-semibold uppercase leading-none text-white opacity-[2%] 2xl:text-[96px]"
-                    style={{
-                        lineHeight: 0.76,
-                        marginLeft: '-0.1em',
-                    }}
-                >
-                    {title}
-                </h2>
-                <Link href={href}>
-                    <h2
-                        className="4xl:text-27xl 3xl:text-25xl 2xl:text-21xl hover:bg-gradient-desktop relative z-10 w-fit text-[80px] font-medium uppercase leading-none text-white hover:bg-clip-text hover:text-transparent"
-                        style={{
-                            lineHeight: 0.8,
-                            marginLeft: '-0.1em',
-                        }}
-                    >
-                        {title}
-                    </h2>
-                </Link>
-            </div>
-        )
-    }
+    const shadowTitle = (
+        <h2
+            className="4xl:text-[7.5rem] 3xl:text-[7rem] absolute bottom-0 z-0 text-[160px] font-semibold uppercase leading-none text-white opacity-[2%] 2xl:text-[96px]"
+            style={{
+                lineHeight: 0.76,
+                marginLeft: '-0.1em',
+            }}
+        >
+            {title}
+        </h2>
+    )
+
+    const mainTitle = (
+        <h2
+            className={`4xl:text-27xl 3xl:text-25xl 2xl:text-21xl ${
+                href ? 'hover:bg-gradient-desktop hover:bg-clip-text hover:text-transparent' : ''
+            } relative z-10 w-fit text-[80px] font-medium uppercase leading-none text-white`}
+            style={{
+                lineHeight: 0.8,
+                marginLeft: '-0.1em',
+            }}
+        >
+            {title}
+        </h2>
+    )
+
     return (
         <div className="relative">
-            <h2
-                className="4xl:text-[7.5rem] 3xl:text-[7rem] absolute bottom-0 z-0 text-[160px] font-semibold uppercase leading-none text-white opacity-[2%] 2xl:text-[96px]"
-                style={{
-                    lineHeight: 0.76,
-                    marginLeft: '-0.1em',
-                }}
-            >
-                {title}
-            </h2>
-            <h2
-                className="4xl:text-27xl 3xl:text-25xl 2xl:text-21xl relative z-10 w-fit text-[80px] font-medium uppercase leading-none text-white"
-                style={{
-                    lineHeight: 0.8,
-                    marginLeft: '-0.1em',
-                }}
-            >
-                {title}
-            </h2>
+            {shadowTitle}
+            {href ? <Link href={href}>{mainTitle}</Link> : mainTitle}
         </div>
     )
 }
