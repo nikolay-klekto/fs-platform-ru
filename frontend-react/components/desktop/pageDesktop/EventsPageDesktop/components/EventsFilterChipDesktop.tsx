a420949c02402910ed0fd0e242160dc32dabefa1
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-07-29 12:51:07 +0500

    FF-204-refactor: update EventsFilterChipDesktop with responsive paddings and font sizes

--- frontend-react/components/desktop/pageDesktop/EventsPageDesktop/components/EventsFilterChipDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/EventsPageDesktop/components/EventsFilterChipDesktop.tsx
@@ -8,9 +8,9 @@ interface IEventsFilterChipDesktop{
 
 const EventsFilterChipDesktop: React.FC<IEventsFilterChipDesktop> = ({ label, onRemove }) => {
     return (
-        <div className="flex items-center rounded-[50px] bg-[#ffffff1a] py-4 pl-[45px] pr-[45px] text-white">
-            <span className="font-semibold text-5xl">{label}</span>
-            <button onClick={onRemove} className="ml-10 flex items-center justify-center">
+        <div className="flex items-center rounded-[50px] bg-[#ffffff1a] py-4 pl-[45px] pr-[45px] 3xl:pl-[40px] 3xl:pr-[40px] 2xl:pl-[35px] 2xl:pr-[35px] text-white">
+            <span className="font-semibold text-5xl 3xl:text-4xl 2xl:text-3xl">{label}</span>
+            <button onClick={onRemove} className="ml-10 3xl:ml-8 2xl:ml-6 flex items-center justify-center">
                 <CloseIconDesktop className="h-3 w-3" />
             </button>
         </div>
