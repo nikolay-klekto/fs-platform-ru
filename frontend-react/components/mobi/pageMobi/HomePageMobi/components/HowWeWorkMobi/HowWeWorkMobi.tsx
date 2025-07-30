91b15698a3c6ba8ba76c2523e94f8ae3ae0677f0
EveLine15 <nnasta788@gmail.com>
2025-07-30 12:40:13 +0300

    style: button in  howWeWork section

--- frontend-react/components/mobi/pageMobi/HomePageMobi/components/HowWeWorkMobi/HowWeWorkMobi.tsx
+++ frontend-react/components/mobi/pageMobi/HomePageMobi/components/HowWeWorkMobi/HowWeWorkMobi.tsx
@@ -12,7 +12,7 @@ const HowWeWorkMobi: React.FC = () => {
             <h2 className="text32px_mobi sm_xl:mb-[24px] sm_l:mb-[24px] sm_s:mb-[24px] mb-[40px] font-medium uppercase text-white sm:mb-[24px] md:mb-[15px]">
                 Как мы работаем
             </h2>
-            <div className="relative px-5">
+            <div className="relative md:px-5">
                 {(showList ? content.slice(0, 2) : content).map((item) => (
                     <div key={item.id}>
                         <div className="text-9xl font-normal text-[#353652] md:text-[clamp(2rem,10vw,4rem)]">
@@ -32,21 +32,23 @@ const HowWeWorkMobi: React.FC = () => {
                     </div>
                 ))}
                 {showList && (
-                    <div
-                        className="absolute bottom-0 left-0 flex h-[100px] w-full items-end justify-center bg-[#353652]"
-                        style={{
-                            background:
-                                'linear-gradient(to bottom, rgba(16, 16, 48, 0.2) 0%, rgba(16, 16, 48, 0.8) 30%,  rgba(16, 16, 48, 1) 100%)',
-                        }}
-                    >
-                        <Button
-                            onClick={() => setShowList(false)}
-                            variant="select_mobi"
-                            size="home_select_mobi"
-                            className="border-[3px] text-[15px] font-medium"
+                    <div className="mb-[90px] md:mb-[160px]">
+                        <div
+                            className="absolute bottom-[-50px] left-0 flex h-[180px] w-full items-end justify-center bg-[#353652] md:bottom-[-90px] md:h-[300px]"
+                            style={{
+                                background:
+                                    'linear-gradient(to bottom, rgba(16, 16, 48, 0.2) 0%, rgba(16, 16, 48, 0.8) 30%,  rgba(16, 16, 48, 1) 100%)',
+                            }}
                         >
-                            <span className="text-[15px] md:text-8xl">Развернуть</span>
-                        </Button>
+                            <Button
+                                onClick={() => setShowList(false)}
+                                variant="select_mobi"
+                                size="home_select_mobi"
+                                className="w-1/2 border-[3px] text-[15px] font-medium"
+                            >
+                                <span className="text-[15px] md:text-[clamp(20px,4vw,26px)]">Развернуть</span>
+                            </Button>
+                        </div>
                     </div>
                 )}
             </div>
