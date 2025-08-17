47aad7133766e7cc09940fdd348eb0915eb9e8c6
Christina-888 <christinabykadorova@gmail.com>
2025-08-17 23:56:59 +0300

    add images to profession cards

--- frontend-react/components/desktop/pageDesktop/ProfessionsPageDesktop/components/ProfessionCardPageDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/ProfessionsPageDesktop/components/ProfessionCardPageDesktop.tsx
@@ -7,32 +7,31 @@ interface IProfessionCard {
     profession: string
     image: string
     price: string
-    category?: string
 }
 
-const ProfessionCardPageDesktop: React.FC<IProfessionCard> = ({ image, profession, price }) => {
+const ProfessionCard: React.FC<IProfessionCard> = ({ image, profession, price }) => {
+    const imagePath = `/images/${image}` //Добавила путь к карточкам
+
     return (
-        <>
-            <Card
-                variant={'profession_page_desktop'}
-                size={'profession_page_desktop'}
-                style={{ backgroundImage: `url(${image})` }}
-            >
-                <CardTitle variant={'profession_page_desktop'} size={'profession_page_desktop'} rounded={'rounded_25'}>
-                    {profession}
-                </CardTitle>
-                <CardFooter variant={'profession_page_desktop'} size={'profession_page_desktop'} rounded={'rounded_25'}>
-                    <p className="text-7xl leading-[40px]  tracking-normal text-[#878797] 2xl:text-3xl 3xl:text-4xl  4xl:text-4xl">
-                        от{' '}
-                        <span className="bg-gradient-desktop bg-clip-text text-8xl text-transparent 2xl:text-4xl 3xl:text-5xl 4xl:text-6xl">
-                            {price} BYN/
-                        </span>
-                        неделя
-                    </p>
-                </CardFooter>
-            </Card>
-        </>
+        <Card
+            variant={'profession_page_desktop'}
+            size={'profession_page_desktop'}
+            style={{ backgroundImage: `url(${imagePath})` }}
+        >
+            <CardTitle variant={'profession_page_desktop'} size={'profession_page_desktop'} rounded={'rounded_25'}>
+                {profession}
+            </CardTitle>
+            <CardFooter variant={'profession_page_desktop'} size={'profession_page_desktop'} rounded={'rounded_25'}>
+                <p className="text-7xl leading-[40px] tracking-normal text-[#878797] 2xl:text-4xl 3xl:text-5xl 4xl:text-6xl">
+                    от{' '}
+                    <span className="bg-gradient-desktop bg-clip-text text-8xl text-transparent 2xl:text-4xl 3xl:text-5xl 4xl:text-6xl">
+                        {price} BYN/
+                    </span>
+                    неделя
+                </p>
+            </CardFooter>
+        </Card>
     )
 }
 
-export default ProfessionCardPageDesktop
+export default ProfessionCard
