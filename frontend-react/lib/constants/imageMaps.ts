70395bc7bb350da1846c9f4e5a4cfba3ed19e001
Christina-888 <christinabykadorova@gmail.com>
2025-08-23 11:45:21 +0300

    ff-227-fix: fix professions in imagemaps.ts

--- frontend-react/lib/constants/imageMaps.ts
+++ frontend-react/lib/constants/imageMaps.ts
@@ -5,14 +5,19 @@ export const imageMaps = {
         Epam: '79.webp',
     },
     professions: {
-        Сбербанк: '45.webp',
-        Innowise: '78.webp',
-        Epam: '79.webp',
+        'бизнес-аналитик': 'profession_1.webp',
+        'маркетолог': 'profession_2.webp',
+        'программист': 'profession_3.webp',
+        'финансист': 'profession_4.webp',
+        'default': 'default.webp',
     },
 } as const
 export type ImageMapCategory = keyof typeof imageMaps
 export type ImageMapKey<T extends ImageMapCategory> = keyof (typeof imageMaps)[T]
 
 export function getImagePath<T extends ImageMapCategory>(category: T, name: string): string {
-    return `/api/photo/${category}/facade/${imageMaps[category][name as ImageMapKey<T>] ?? 'default.webp'}`
+    const normalizedName = name.toLowerCase().trim().replace(/\s+/g, ' ')
+    const fileName = imageMaps[category][normalizedName as ImageMapKey<T>] ?? 'default.webp'
+
+    return `/api/photo/${category}/facade/${fileName}`
 }
