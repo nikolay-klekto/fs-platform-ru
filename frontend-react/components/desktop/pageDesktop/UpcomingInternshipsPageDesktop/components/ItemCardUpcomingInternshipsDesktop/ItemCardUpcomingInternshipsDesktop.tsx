1e34817234b6ae0ba458ea869ad938e397cbe514
Katerina-99 <rakitinakaty555@gmail.com>
2025-08-25 19:34:00 +0100

    FF-243-fix:change card internship with ui card,fix interface misprint,fix alt for image,replace link to button

--- frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/ItemCardUpcomingInternshipsDesktop/ItemCardUpcomingInternshipsDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/ItemCardUpcomingInternshipsDesktop/ItemCardUpcomingInternshipsDesktop.tsx
@@ -1,8 +1,10 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
 
-interface IIntershipsCard {
+interface IInternshipsCard {
     title: string
     subtitle: string
     image: string
@@ -14,7 +16,7 @@ interface IIntershipsCard {
     mapImage: string
 }
 
-const ItemCardUpcomingInternshipsDesktop: React.FC<IIntershipsCard> = ({
+const ItemCardUpcomingInternshipsDesktop: React.FC<IInternshipsCard> = ({
     title,
     subtitle,
     image,
@@ -26,25 +28,28 @@ const ItemCardUpcomingInternshipsDesktop: React.FC<IIntershipsCard> = ({
     mapImage,
 }) => {
     return (
-        <div className="flex flex-col rounded-[50px] w-[1069px] min-w-[700px] bg-white/10 p-[40px]">
-            <div className="mb-[40px] flex items-center gap-[35px]">
+        <Card className="flex flex-col w-[1069px] min-w-[700px] bg-white/10 p-[40px]">
+            <CardContent className="mb-[40px] p-0 flex items-center gap-[35px]">
                 <div className="pl-[45px]">
-                    <h2 className="text32px_desktop text-gradient_desktop_custom mb-[10px] font-medium uppercase">
-                        {title}
-                    </h2>
-                    <p className="text18px_desktop custom-grey mb-[20px] font-medium">{subtitle}</p>
-                    <p className="text18px_desktop mb-[20px] font-medium">
+                    <CardHeader className="space-y-0 p-0 m-0">
+                        <CardTitle className="text-[32px] text32px_desktop text-gradient_desktop_custom rounded-none pb-[10px] font-medium uppercase">
+                            {title}
+                        </CardTitle>
+                    </CardHeader>
+                    <p className="text-[18px] text18px_desktop custom-grey pb-[20px] font-medium">{subtitle}</p>
+
+                    <p className="text-[18px] text18px_desktop pb-[20px] font-medium">
                         {date} в {time}
                     </p>
-                    <p className="text24px_desktop mb-[23px] font-medium">
+                    <p className="text-[24px] text24px_desktop pb-[23px] font-medium">
                         {city}, {place}
                     </p>
-                    <p className="text15px_desktop font-medium custom-grey">{note}</p>
+                    <p className=" text-[15px] text15px_desktop font-medium custom-grey">{note}</p>
                 </div>
                 <div className="relative w-full max-w-[332px] aspect-[332/328] rounded-[50px] overflow-hidden">
-                    <Image src={image} fill alt={title} className="object-cover object-center" />
+                    <Image src={image} fill alt={'Фото офиса EPAM'} className="object-cover object-center" />
                 </div>
-            </div>
+            </CardContent>
             <div className="relative w-full h-[180px] rounded-[25px] mb-[40px] overflow-hidden">
                 <Image src={mapImage} alt="Карта превью" fill className="rounded-[26px] object-cover" />
                 <div className="absolute inset-0 bg-[#1F203F] opacity-90 rounded-[25px] flex items-center justify-center">
@@ -53,10 +58,15 @@ const ItemCardUpcomingInternshipsDesktop: React.FC<IIntershipsCard> = ({
                     </a>
                 </div>
             </div>
-            <div className="self-end text20px_desktop custom-grey underline hover:text-gray-200">
-                <a href="/#">Отменить заказ</a>
-            </div>
-        </div>
+            <CardFooter className="self-end ">
+                <Button
+                    variant="link"
+                    className="text20px_desktop text-[#878797] underline underline-offset-4 hover:text-gray-200"
+                >
+                    Отменить заказ
+                </Button>
+            </CardFooter>
+        </Card>
     )
 }
 
