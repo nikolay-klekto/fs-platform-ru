78802e56dd3dee2baa21b3cd7c00d59355098bb7
Katerina-99 <rakitinakaty555@gmail.com>
2025-08-26 13:46:12 +0100

    FF-243-fix:change render cards,change buttons,remove custom class,fix style for card item

--- frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/ItemCardUpcomingInternshipsDesktop/ItemCardUpcomingInternshipsDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/ItemCardUpcomingInternshipsDesktop/ItemCardUpcomingInternshipsDesktop.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Image from 'next/image'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
 
 interface IInternshipsCard {
     title: string
@@ -30,42 +29,47 @@ const ItemCardUpcomingInternshipsDesktop: React.FC<IInternshipsCard> = ({
     mapUrl,
 }) => {
     return (
-        <Card className="flex flex-col w-full max-w-[1069px] min-w-[700px] bg-custom-upcoming-internship-card p-[40px]">
-            <CardContent className="mb-[40px] p-0 flex items-center gap-[35px]">
+        <Card className="flex flex-col w-full max-w-[1069px] min-w-[700px] bg-[#FFFFFF1A] p-[40px]">
+            <CardContent className="mb-[40px] p-0 flex items-center gap-[67px]">
                 <div className="pl-[45px]">
-                    <CardHeader className="space-y-0 p-0 m-0">
+                    <CardHeader className="flex-row space-y-0 p-0 m-0">
                         <CardTitle className="text-[32px] text32px_desktop text-gradient_desktop_custom rounded-none pb-[10px] font-medium uppercase">
                             {title}
                         </CardTitle>
                     </CardHeader>
-                    <p className="text-[18px] text18px_desktop custom-grey pb-[20px] font-medium">{subtitle}</p>
+                    <p className="text18px_desktop custom-grey pb-[20px] font-medium">{subtitle}</p>
 
-                    <p className="text-[18px] text18px_desktop pb-[20px] font-medium">
+                    <p className="text18px_desktop pb-[20px] font-medium">
                         {date} в {time}
                     </p>
-                    <p className="text-[24px] text24px_desktop pb-[23px] font-medium">
+                    <p className="text24px_desktop pb-[23px] font-medium">
                         {city}, {place}
                     </p>
-                    <p className=" text-[15px] text15px_desktop font-medium custom-grey">{note}</p>
+                    <p className="text15px_desktop font-medium custom-grey">{note}</p>
                 </div>
                 <div className="relative w-full max-w-[332px] aspect-[332/328] rounded-[50px] overflow-hidden">
                     <Image src={image} fill alt={'Фото офиса EPAM'} className="object-cover object-center" />
                 </div>
             </CardContent>
             <div className="relative w-full h-[180px] rounded-[25px] mb-[40px] overflow-hidden">
-                <Image src={mapImage} alt="Изображение карты" fill className="rounded-[26px] object-cover" />
-                <div className="absolute inset-0 bg-[#1F203F] opacity-90 rounded-[25px] flex items-center justify-center">
-                    <Button variant="link" className="text-[24px] text-[#878797] hover:no-underline">
+                <Image
+                    src={mapImage}
+                    alt="Изображение карты"
+                    fill
+                    className="object-cover transform-gpu translate-full"
+                />
+                <div className="absolute inset-0 top-0 left-0 bg-[#1F203F] opacity-90 flex items-center justify-center">
+                    <button className="text24px_desktop text-[#878797]">
                         <a href={mapUrl} target="_blank" rel="noopener noreferrer">
                             Смотреть на карте
                         </a>
-                    </Button>
+                    </button>
                 </div>
             </div>
             <CardFooter className="self-end">
-                <Button variant="link" className="text-[20px] text-[#878797] p-0 hover:no-underline">
+                <button className="text20px_desktop text-[#878797]">
                     <span className="border-[#878797] border-b-[1px]">Отменить заказ</span>
-                </Button>
+                </button>
             </CardFooter>
         </Card>
     )
--- frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/UpcomingInternshipDesktop/UpcomingInternshipDesktop.tsx
+++ frontend-react/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/components/UpcomingInternshipDesktop/UpcomingInternshipDesktop.tsx
@@ -4,30 +4,37 @@ import Link from 'next/link'
 import { content } from '@/components/desktop/pageDesktop/UpcomingInternshipsPageDesktop/contentUpcomingInternshipsPageDesktop/content'
 import { Button } from '@/components/ui/button'
 import ItemCardUpcomingInternshipsDesktop from '../ItemCardUpcomingInternshipsDesktop/ItemCardUpcomingInternshipsDesktop'
+import { useIsAuth } from '@/hooks/useIsAuth'
 
 const UpcomingInternshipDesktop: React.FC = () => {
-    const isEmpty = content.length === 0
+    const isAuth = useIsAuth()
+    const hasInternships = content && content.length > 0
 
-    if (isEmpty) {
-        return (
-            <div className="flex flex-col items-center pb-[370px]">
-                <p className="mb-4 text-7xl font-medium leading-[40px] text-[#353652]">Предстоящих стажировок нет</p>
-                <Link href={'/professions'}>
-                    <Button variant={'send_btn_desktop'} size={'send_btn_desktop'}>
-                        Выбрать профессию
-                    </Button>
-                </Link>
-            </div>
-        )
-    }
     return (
-        <div className="grid">
-            <div className="mx-auto flex flex-col justify-between gap-[80px] pb-[88px] 2xl:pb-[40px]">
-                {content.map((item) => (
-                    <ItemCardUpcomingInternshipsDesktop key={item.id} {...item} />
-                ))}
-            </div>
-        </div>
+        <>
+            {isAuth && !hasInternships && (
+                <div className="flex flex-col items-center pb-[370px]">
+                    <p className="mb-4 text-7xl font-medium leading-[40px] text-[#353652]">
+                        Предстоящих стажировок нет
+                    </p>
+                    <Link href={'/professions'}>
+                        <Button variant={'send_btn_desktop'} size={'send_btn_desktop'}>
+                            Выбрать профессию
+                        </Button>
+                    </Link>
+                </div>
+            )}
+
+            {hasInternships && (
+                <div className="grid">
+                    <div className="mx-auto flex flex-col justify-between gap-[80px] pb-[88px] 2xl:pb-[40px]">
+                        {content.map((item) => (
+                            <ItemCardUpcomingInternshipsDesktop key={item.id} {...item} />
+                        ))}
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 export default UpcomingInternshipDesktop
